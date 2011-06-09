5bd9908868f92be0607c14f493ffa4c8b16b273d
Matt Webb <mattrw89@gmail.com>
2011-06-09 15:10:13 -0400

    try to fix production server oauth/admin issue

--- config/application.rb
+++ config/application.rb
@@ -54,5 +54,16 @@ module Ma
     # Enable the asset pipeline
     config.assets.enabled = true
     
+    config.after_initialize do
+      # integrate with devise
+      config.oauth.authenticator = lambda do |email, password|
+        user = User.find_for_database_authentication(:email => email)
+        user if user && user.valid_password?(password)
+        user.userID
+      end
+      config.oauth.param_authentication = TRUE
+      Rack::OAuth2::Server::Admin.set :client_id, "2"
+      Rack::OAuth2::Server::Admin.set :client_secret, "e6f0bc02c1236f3d4cde6a4fd45e181569a8abf45ce17a3dba2fd88fe55722b6"
+    end
   end
 end
