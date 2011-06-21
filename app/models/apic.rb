4eb12b39819f1cf58e0daf153bb0a1e5b61f4e09
Matt Webb <mattrw89@gmail.com>
2011-06-21 15:46:36 -0400

    add email and phone number to allowed person fields

--- app/models/apic.rb
+++ app/models/apic.rb
@@ -1,7 +1,7 @@
 class Apic 
   API_ALLOWABLE_FIELDS = {
     :v1 => {
-      :people => ["first_name", "last_name", "name", "id", "location", "birthday","locale","gender","interests","education","fb_id","picture", "status", "request_org_id", "assignment", "organization_membership"],
+      :people => ["first_name", "last_name", "name", "id", "location", "birthday","locale","gender","interests","education","fb_id","picture", "status", "request_org_id", "assignment", "organization_membership","phone_number","email_address"],
       :school => [""],
       :friends => ["uid", "name", "provider"],
       :contacts => ["all"],
