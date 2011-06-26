4c1f95b36e5a9d01b5c87598de20a5fec610c12b
Matt Webb <mattrw89@gmail.com>
2011-06-26 15:50:08 -0400

    Retry the helper fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -74,7 +74,7 @@ class ApplicationController < ActionController::Base
   helper_method :unassigned_people
   
   def unassigned_people_api(people,organization)
-    @unassigned_people ||= people.joins("INNER JOIN organization_memberships ON organization_memberships.person_id = #{Person.table_name}.#{Person.primary_key} AND organization_memberships.organization_id = #{organization.id} AND organization_memberships.role = 'contact' RIGHT JOIN contact_assignments ON contact_assignments.person_id = #{Person.table_name}.#{Person.primary_key} AND contact_assignments.organization_id = #{@organization.id}")
+    @unassigned_people ||= people.joins("INNER JOIN organization_memberships ON organization_memberships.person_id = #{Person.table_name}.#{Person.primary_key} AND organization_memberships.organization_id = #{organization.id} AND organization_memberships.role = 'contact' LEFT OUTER JOIN contact_assignments ON contact_assignments.person_id = #{Person.table_name}.#{Person.primary_key} AND contact_assignments.organization_id = #{@organization.id}").where('contact_assignments.id' => nil)
   end
   helper_method :unassigned_people_api
   
