2ff22dcc327c9058acd0cde40d2fade6e11a881b
Josh Starcher <josh.starcher@gmail.com>
2011-07-08 16:09:33 -0400

    more merge fixes

--- app/models/ccc/simplesecuritymanager_user.rb
+++ app/models/ccc/simplesecuritymanager_user.rb
@@ -1,5 +1,20 @@
 class Ccc::SimplesecuritymanagerUser < ActiveRecord::Base
   set_primary_key :userID
   set_table_name 'simplesecuritymanager_user'
-  has_many :sp_users, class_name: 'Ccc::SpUser'
+  has_one :sp_user, class_name: 'Ccc::SpUser'
+  has_one :mpd_user, class_name: 'Ccc::MpdUser', dependent: :destroy
+  
+  def merge(other)
+    if other.mpd_user and mpd_user
+      mpd_user.merge(other.mpd_user)
+    elsif other.mpd_user
+      other.mpd_user.user_id = fk_ssmUserID
+    end
+  
+		if other.pr_user and pr_user
+			other.pr_user.destroy				
+		elsif other.pr_user
+			other.pr_user.ssm_id = fk_ssmUserID
+		end
+  end
 end
