02fa9218a61958cebf99e213d2a348d6d8e1c8f9
Josh Starcher <josh.starcher@gmail.com>
2011-07-13 13:49:54 -0400

    send an approval email when a keyword is approved

--- app/mailers/keyword_request_mailer.rb
+++ app/mailers/keyword_request_mailer.rb
@@ -1,10 +1,15 @@
 class KeywordRequestMailer < ActionMailer::Base
-  default from: "no-reply@campuscrusadeforchrist.com"
+  default from: "support@missionhub.com"
   layout 'email'
   
   def new_keyword_request(keyword)
     @keyword = keyword
     mail(to: 'programmers@cojourners.com',
          subject: 'New sms keyword request')
+  end
+  
+  def notify_user(keyword)
+    @keyword = keyword
+    mail(to: keyword.user.person.email, subject: "Keyword '#{keyword}' was approved!")
   end
 end
