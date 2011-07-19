558d9fced9b94561b9a77566a29e83fe45e95628
Josh Starcher <josh.starcher@gmail.com>
2011-07-19 09:46:39 -0400

    nicer logging for when facebook auth fails

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,11 +5,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     omniauth = env["omniauth.auth"]
     session[:fb_token] = omniauth["credentials"]["token"] if omniauth['provider'] == 'facebook'
    
-    if @user.persisted?
+    if @user && @user.persisted?
       sign_in_and_redirect @user, event: :authentication
     else
+      # There was a problem logging this person in
+      HoptoadNotifier.notify(
+        :error_class => "FacebookLoginError",
+        :error_messsage => "Facebook Login Error",
+        :parameters => env["omniauth.auth"]
+      )
       session["devise.facebook_data"] = env["omniauth.auth"]
-      redirect_to communities_path
+      redirect_to '/'
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
   
   def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
     data = access_token['extra']['user_hash']
-    
+    return nil unless data["email"].present?
     user = nil
     authentication = nil
     
