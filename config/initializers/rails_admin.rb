388b9bed4a9d297310d253ca7d40ee12fcecdf41
Josh Starcher <josh.starcher@gmail.com>
2011-08-05 13:23:17 -0400

    update gems

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,3 +1,21 @@
 RailsAdmin.config do |config|
   config.included_models = ["Organization", "SmsKeyword"]
+  if (Rails.env.production?)
+    RailsAdmin::AbstractModel.all_models = nil
+    RailsAdmin::AbstractModel.all_abstract_models = nil
+    RailsAdmin::AbstractModel.all # rebuild model arrays
+  end
+end
+
+module RailsAdmin
+  module Config
+    class Model < RailsAdmin::Config::Base
+
+      # monkey patch to get around stale @excluded instance variable
+      def excluded?
+        !RailsAdmin::AbstractModel.all.map(&:model).include?(abstract_model.model)
+      end
+
+    end
+  end
 end
