c7b43dfbe3edca8457e8abde386df438d626d6c4
Josh Starcher <josh.starcher@gmail.com>
2011-08-16 07:42:39 -0400

    don't barf when trying to link a facebook account twice

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,20 +13,27 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   protected
   
   def facebook_login
-    @user = User.find_for_facebook_oauth(env["omniauth.auth"], current_user)
-    omniauth = env["omniauth.auth"]
-    session[:fb_token] = omniauth["credentials"]["token"]
+    begin
+      User.transaction do
+        @user = User.find_for_facebook_oauth(env["omniauth.auth"], current_user)
+        omniauth = env["omniauth.auth"]
+        session[:fb_token] = omniauth["credentials"]["token"]
    
-    if @user && @user.persisted?
-      sign_in_and_redirect(@user, event: :authentication) and return
-    else
-      # There was a problem logging this person in
-      HoptoadNotifier.notify(
-        :error_class => "FacebookLoginError",
-        :error_messsage => "Facebook Login Error",
-        :parameters => env["omniauth.auth"]
-      )
-      session["devise.facebook_data"] = env["omniauth.auth"]
+        if @user && @user.persisted?
+          sign_in_and_redirect(@user, event: :authentication) and return
+        else
+          # There was a problem logging this person in
+          HoptoadNotifier.notify(
+            :error_class => "FacebookLoginError",
+            :error_messsage => "Facebook Login Error",
+            :parameters => env["omniauth.auth"]
+          )
+          session["devise.facebook_data"] = env["omniauth.auth"]
+          redirect_to '/' and return
+        end
+      end
+    rescue => e
+      notify_hoptoad(e)
       redirect_to '/' and return
     end
   end
