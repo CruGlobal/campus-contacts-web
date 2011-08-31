94c8bee4e8a9c354aa20b5bfa26a0db761a245bf
Josh Starcher <josh.starcher@gmail.com>
2011-08-31 11:45:36 -0400

    back to memcache-client

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-require 'action_dispatch/middleware/session/dalli_store'
-Mh::Application.config.session_store :dalli_store, :memcache_server => ['127.0.0.1'], :namespace => 'sessions', :key => '_mh_session', :expire_after => 2.days
+# require 'action_dispatch/middleware/session/dalli_store'
+# Mh::Application.config.session_store :dalli_store, :memcache_server => ['127.0.0.1'], :namespace => 'sessions', :key => '_mh_session', :expire_after => 2.days
 # Mh::Application.config.session_store :cookie_store, key: '_mh_session'
-# Mh::Application.config.session_store :mem_cache_store, key: '_mh_session'
+Mh::Application.config.session_store :mem_cache_store, key: '_mh_session'
 # Mh::Application.config.session_store :active_record, key: '_mh_session'
