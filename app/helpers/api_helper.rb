1063e97817864311fe5d50c693ec90b072e239af
Josh Starcher <josh.starcher@gmail.com>
2011-09-22 13:43:31 -0400

    you can have contacts even if there are no keywords

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -95,10 +95,11 @@ module ApiHelper
       @keywords = SmsKeyword.find_all_by_keyword(params[:keyword])
     elsif (params[:org].present? || params[:org_id].present?)
       org_id = params[:org].present? ? params[:org].to_i : params[:org_id].to_i
-      @keywords = SmsKeyword.find_all_by_organization_id(org_id)
+      @keywords = Organization.find_by_id(org_id).try(:self_and_children_keywords) || []
     elsif params[:keyword_id].present?
-      @keywords = SmsKeyword.find_all_by_id(params[:id])
-    else @keywords = SmsKeyword.find_all_by_organization_id(current_organization.id)
+      @keywords = SmsKeyword.find_all_by_id(params[:keyword_id])
+    else 
+      @keywords = current_organization.self_and_children_keywords
     end
   end
   
