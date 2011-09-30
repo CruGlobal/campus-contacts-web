deef3a41d39f73980f2ce9e20b31370c401db625
Josh Starcher <josh.starcher@gmail.com>
2011-09-30 14:07:29 -0400

    when moving a contact, we need to also move the comments

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -120,4 +120,10 @@ class Organization < ActiveRecord::Base
     OrganizationalRole.where(person_id: person_id, organization_id: id, role_id: Role::CONTACT_ID).first.try(:destroy)
   end
   
+  def move_contact(person, to_org)
+    remove_contact(person)
+    to_org.add_contact(person)
+    FollowupComment.where(organization_id: id, contact_id: person.id).update_all(organization_id: to_org.id)
+  end
+  
 end
