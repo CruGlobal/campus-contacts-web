9e3db57a06a37f6c92a17a5188f5840a85e15f75
Josh Starcher <josh.starcher@gmail.com>
2011-10-28 13:17:20 -0400

    add meeting time to group details

--- app/presenters/group_presenter.rb
+++ app/presenters/group_presenter.rb
@@ -6,4 +6,24 @@ class GroupPresenter < DelegatePresenter::Base
   def leaders_names
     leaders.collect(&:name).join(', ')
   end
+  
+  def meets_at
+    case meets
+    when 'sporadically' then I18n.t('groups.show.sporadically')
+    when 'weekly'
+      I18n.t('groups.show.weekly', day: I18n.t('date.day_names')[meeting_day], start_time: human_start_time, end_time: human_end_time)
+    when 'monthly'
+      I18n.t('groups.show.monthly', day: meeting_day.ordinalize, start_time: human_start_time, end_time: human_end_time)
+    else
+      ''
+    end
+  end
+  
+  def human_start_time
+    I18n.l(Time.now.beginning_of_day + start_time, format: :time_only)
+  end
+  
+  def human_end_time
+    I18n.l(Time.now.beginning_of_day + end_time, format: :time_only)
+  end
 end
