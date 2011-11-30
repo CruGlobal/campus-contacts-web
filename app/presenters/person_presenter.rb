5180a84ad1341d0e3418092a77a4a4254996cedd
David Sudarma <davidsudarma@David-Sudarmas-iMac.local>
2011-11-30 13:48:56 -0800

    Style changes to contacts page

--- app/presenters/person_presenter.rb
+++ app/presenters/person_presenter.rb
@@ -3,7 +3,7 @@ class PersonPresenter < DelegatePresenter::Base
     phone_numbers.order('`primary` desc').collect do |phone_number| 
       s = h(helpers.number_to_phone(phone_number.number))
       s += " (#{I18n.t('general.' + phone_number.location)})" if phone_number.location.present?
-      s += '*' if phone_number.primary?
+      s += helpers.raw('<span class="primarymark"></span>') if phone_number.primary?
       s
     end.join('<br /> ').html_safe
   end
@@ -11,7 +11,7 @@ class PersonPresenter < DelegatePresenter::Base
   def email_addresses_html
     email_addresses.order('`primary` desc').collect do |email| 
       s = helpers.mail_to(email)
-      s += '*' if email.primary?
+      s += helpers.raw('<span class="primarymark"></span>') if email.primary?
       s
     end.join('<br /> ').html_safe
   end
