6325abdeb36676b62ad8a7277ee39d95711aec2f
Josh Starcher <josh.starcher@gmail.com>
2011-12-13 11:48:22 -0500

    fix oauth admin

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -3,26 +3,26 @@ class OauthController < ApplicationController
   include ApiHelper
   rescue_from Exception, with: :render_json_error    
   def authorize
-      if current_user
-        render :action=>"authorize"
-      else
-        redirect_to :action=>"login", :authorization=>oauth.authorization
-      end
+    if current_user
+      render :action=>"authorize"
+    else
+      redirect_to :action=>"login", :authorization=>oauth.authorization
     end
+  end
 
-    def grant
-      head oauth.grant!(current_user.id)
-    end
+  def grant
+    head oauth.grant!(current_user.id)
+  end
 
-    def deny
-      head oauth.deny!
-    end
+  def deny
+    head oauth.deny!
+  end
+  
+  def done
+    raise AccountSetupRequiredError unless current_organization
+    raise IncorrectPermissionsError unless current_user.person.leader_in?(current_organization)
     
-    def done
-      raise AccountSetupRequiredError unless current_organization
-      raise IncorrectPermissionsError unless current_user.person.leader_in?(current_organization)
-      
-      json_output = '{"status":"done", "code":"' + params[:code] + '"}'
-      render json: json_output
-    end
+    json_output = '{"status":"done", "code":"' + params[:code] + '"}'
+    render json: json_output
+  end
 end
