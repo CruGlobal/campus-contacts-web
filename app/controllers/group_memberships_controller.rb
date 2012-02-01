a0a918b31c64fa3ff60e38b3747167a8d3b6c5a6
Vincent Paca <vincent.paca@gmail.com>
2012-02-01 20:57:55 +0800

    modified how group_memberships are handled and added corresponding tests.

--- app/controllers/group_memberships_controller.rb
+++ app/controllers/group_memberships_controller.rb
@@ -1,27 +1,45 @@
 class GroupMembershipsController < ApplicationController
 
   def create
-
     @group = current_organization.groups.find(params[:group_id]) 
     if params[:from_add_member_screen] == "true"
-      return false unless has_permission
       @persons = Person.find(params[:person_id])
-      @group_membership = @group.group_memberships.find_or_initialize_by_person_id(@persons.id)
-      @group_membership.role = params[:role]
-      @group_membership.save
-    else
-      @persons = Person.find(params[:person_id].split(","))
-      return false unless has_permission
-      @persons.each do |person|
-        @group_membership = @group.group_memberships.find_or_initialize_by_person_id(person.id)
+      if has_permission
+        @group_membership = @group.group_memberships.find_or_initialize_by_person_id(@persons.id)
         @group_membership.role = params[:role]
         @group_membership.save
-      end
-    end
 
-    respond_to do |wants|
-      wants.html { render :nothing => true }
-      wants.js
+        respond_to do |wants|
+          wants.html { render :nothing => true }
+          wants.js
+        end
+      else
+        respond_to do |wants|
+          wants.html { render :nothing => true }
+          wants.js { render 'failed.js.erb' }
+        end
+        return false
+      end
+    else
+      @persons = Person.find(params[:person_id].split(","))
+      if has_permission
+        @persons.each do |person|
+          @group_membership = @group.group_memberships.find_or_initialize_by_person_id(person.id)
+          @group_membership.role = params[:role]
+          @group_membership.save
+        end
+        
+        respond_to do |wants|
+          wants.html { render :nothing => true }
+          wants.js
+        end
+      else
+        respond_to do |wants|
+          wants.html { render :nothing => true }
+          wants.js { render 'failed.js.erb' }
+        end
+        return false
+      end
     end
   end
   
@@ -52,6 +70,7 @@ class GroupMembershipsController < ApplicationController
   protected
     def has_permission
       return true if can?(:manage, current_organization)
+      #return true if can?(:lead, current_organization)
       return true if @group.leaders.include?(current_person)
       return true if @group.list_publicly? && params[:role] == 'interested' && @person == current_person
       return true if @group.public_signup? && params[:role] == 'member' && @person == current_person
--- test/functional/group_memberships_controller_test.rb
+++ test/functional/group_memberships_controller_test.rb
@@ -11,30 +11,65 @@ class GroupMembershipsControllerTest < ActionController::TestCase
       @person2 = Factory(:person)
       @person3 = Factory(:person)
       @person4 = Factory(:person)
-      
-      user = Factory(:user_with_auxs)
-      sign_in user
-      org = Factory(:organization)
-      org.add_admin(user.person)
-      
-      @group = Factory(:group, organization: org)
-      request.session[:current_organization_id] = org.id 
+      @org = Factory(:organization)
+      @group = Factory(:group, organization: @org)
+      request.session[:current_organization_id] = @org.id 
     end
     
-    should "properly assign a single user to a group" do
-      post :create, { :group_id => @group.id, :person_id => @person1.id.to_s, :role => "member" }
-      assert_equal(@group.members.count, 1, "must return correct number of members.")
-      assert(@group.members.include?(@person1), "person must be assigned to group")
+    context "and user has permissions" do
+      setup do
+        user = Factory(:user_with_auxs)
+        sign_in user
+        @org.add_admin(user.person)
+      end
+      
+      should "properly assign a single user to a group" do
+        xhr :post, :create, { :group_id => @group.id, :person_id => @person1.id.to_s, :role => "member", :from_add_member_screen => "true" }
+        assert_equal(assigns(:group), @group)
+        assert_equal(assigns(:persons), @person1)
+        assert_not_nil(assigns(:persons))
+        assert_not_nil(assigns(:group_membership))
+        assert(assigns(:group).members.include? @person1)
+        
+        assert_template('group_memberships/create')
+        assert_response(:success)
+      end
+    
+      should "properly assign multiple users to a group" do
+        person_ids = "#{@person1.id},#{@person2.id},#{@person3.id},#{@person4.id},"
+        xhr :post, :create, { :group_id => @group.id, :person_id => person_ids, :role => "member" }
+      
+        assert_equal(assigns(:group), @group)
+        assert_not_nil(assigns(:persons))
+        assert_equal(4, assigns(:persons).count)
+        assert(assigns(:group).members.include? @person1)
+        assert(assigns(:group).members.include? @person2)
+        assert(assigns(:group).members.include? @person3)
+        assert(assigns(:group).members.include? @person4)
+        
+        assert_template('group_memberships/create')
+        assert_response(:success)
+      end
     end
     
-    should "properly assign multiple users to a group" do
-      person_ids = "#{@person1.id},#{@person2.id},#{@person3.id},#{@person4.id}"
-      post :create, { :group_id => @group.id, :person_id => person_ids, :role => "member" }
-      assert_equal(@group.members.count, 4, "must return correct number of members.")
-      assert(@group.members.include?(@person1), "person must be assigned to group")
-      assert(@group.members.include?(@person2), "person must be assigned to group")
-      assert(@group.members.include?(@person3), "person must be assigned to group")
-      assert(@group.members.include?(@person4), "person must be assigned to group")
+    context "user has no permissions" do
+      setup do
+        user = Factory(:user_with_auxs)
+        sign_in user
+        @org.add_leader(user.person)
+      end
+      
+      should "fail to assign a single user to a group" do
+        xhr :post, :create, { :group_id => @group.id, :person_id => @person1.id.to_s, :role => "member", :from_add_member_screen => "true" }
+        assert_template('group_memberships/failed')
+      end
+      
+      should "fail to assign multiple users to a group" do
+        person_ids = "#{@person1.id},#{@person2.id},#{@person3.id},#{@person4.id},"
+        xhr :post, :create, { :group_id => @group.id, :person_id => person_ids, :role => "member" }
+        
+        assert_template('group_memberships/failed')
+      end
     end
   end
 end
