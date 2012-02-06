6106332642816b0c547f5be79664b5747e074767
neilmarion <neilmarion@neilmarion-notebook.(none)>
2012-02-07 04:57:02 +0800

    Fixed limit of saved_contact_searches table's full_path column string to 4000

--- db/migrate/20120206202605_fix_saved_contact_searches_full_path_string_length.rb
+++ db/migrate/20120206202605_fix_saved_contact_searches_full_path_string_length.rb
@@ -1,5 +1,5 @@
 class FixSavedContactSearchesFullPathStringLength < ActiveRecord::Migration
   def change
-    change_column :saved_contact_searches, :full_path, :string, :limit => 200
+    change_column :saved_contact_searches, :full_path, :string, :limit => 4000
   end
 end
