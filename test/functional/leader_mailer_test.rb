c2cee16a1c28d81721a8261d339041b69510660c
neilmarion <neilmarion@neilmarion-notebook.(none)>
2012-02-08 02:00:08 +0800

    Added testing sending email after leader was added. Re: MH-241

--- test/functional/leader_mailer_test.rb
+++ test/functional/leader_mailer_test.rb
@@ -1,12 +1,16 @@
 require 'test_helper'
 
 class LeaderMailerTest < ActionMailer::TestCase
-  # test "added" do
-  #   mail = LeaderMailer.added
-  #   assert_equal "Added", mail.subject
-  #   assert_equal ["to@example.org"], mail.to
-  #   assert_equal ["from@example.com"], mail.from
-  #   assert_match "Hi", mail.body.encoded
-  # end
+  context "When OrganizationalRole with leader role is created" do
+    should "email the new leader" do
+      user1 = Factory(:user_with_auxs, email: 'test@uscm.org')
+      user2 = Factory(:user_with_auxs, email: 'test2@uscm.org')
+      org = Factory(:organization)
+      puts Factory(:organizational_role, person: user1.person, role: Role.leader, organization: org, :added_by_id => user2.person.id)
+      assert_equal ActionMailer::Base.deliveries.count, 1, "No email is being sent after leader is added."
+      assert_equal ActionMailer::Base.deliveries.first.from.first, user2.email, "Wrong sending email after email is sent after leader is added."
+      assert_equal ActionMailer::Base.deliveries.first.to.first, user1.email, "Wrong receiving email after email is sent after leader is added."
+    end
+  end
 
 end
