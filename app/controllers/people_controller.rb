e414b03762e10c09fabba3c1d5314f642f4d4e6c
neilmarion <neilmarion@neilmarion-notebook.(none)>
2012-02-28 02:03:08 +0800

    Fixing updating roles

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -307,12 +307,10 @@ class PeopleController < ApplicationController
     organizational_role_ids = person.organizational_roles.where(organization_id: current_organization.id).collect { |role| role.role_id.to_s }
 
     #if role_ids.length (new roles) is less than old roles. i.e. there is a role that is going to be deleted
-    #The purpose of this code block is to avoid emailing (that a Person has just become a leader) a Person if he is already a leader before this roles update
-    if organizational_role_ids.length > role_ids.length
-      organizational_role_ids = organizational_role_ids - role_ids
-      organizational_roles = person.organizational_roles.where(organization_id: current_organization.id).collect { |role| role.id if organizational_role_ids.include?(role.role_id.to_s) }
-      OrganizationalRole.delete(organizational_roles)
-    end
+    #The purpose of this code block is to avoid emailing (that a Person has just become a leader) a Person if he is already a leader before this roles updateh
+    organizational_role_ids = organizational_role_ids - role_ids
+    organizational_roles = person.organizational_roles.where(organization_id: current_organization.id).collect { |role| role.id if organizational_role_ids.include?(role.role_id.to_s) }
+    OrganizationalRole.delete(organizational_roles)    
 
     role_ids.uniq.each_with_index do |role_id, index|
         begin
