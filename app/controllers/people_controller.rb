99d763fba033c2f596a898687fe092b93bcbc430
neilmarion <neilmarion@neilmarion-notebook.(none)>
2012-03-20 02:18:34 +0800

    Combined sorted results of People with highest role default and highest role non-default

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -506,9 +506,8 @@ class PeopleController < ApplicationController
       @all_people = @q.result(distinct: false).order(params[:q] && params[:q][:s] ? params[:q][:s] : sort_by)
       if !params[:q].nil? && params[:q][:s].include?("role_id")
         order = params[:q][:s].include?("asc") ? params[:q][:s].gsub("asc", "desc") : params[:q][:s].gsub("desc", "asc")
-        @all_people = @q.result(distinct: false).order_by_highest_default_role(order)
-        @all_people = @all_people.all.uniq_by { |a| a.id }
-        #@all_people = @all_people.reverse if params[:q][:s].include?("desc")
+        @all_people = @q.result(distinct: false).order_by_highest_default_role(order) + @q.result(distinct: false).order_alphabetically_by_non_default_role(order)
+        @all_people = @all_people.uniq_by { |a| a.id }
       end
       
       @people = Kaminari.paginate_array(@all_people).page(params[:page])
--- app/models/person.rb
+++ app/models/person.rb
@@ -61,13 +61,15 @@ class Person < ActiveRecord::Base
   scope :order_by_highest_default_role, lambda { |order| {
     :select => "ministry_person.*",
     :joins => "JOIN organizational_roles ON ministry_person.personID = organizational_roles.person_id JOIN roles ON organizational_roles.role_id = roles.id",
+    :conditions => "roles.i18n IN #{Role.default_roles_for_field_string(order.include?("asc") ? Role::DEFAULT_ROLES : Role::DEFAULT_ROLES.reverse)}",
     :order => "FIELD #{Role.i18n_field_plus_default_roles_for_field_string(order.include?("asc") ? Role::DEFAULT_ROLES : Role::DEFAULT_ROLES.reverse)}"
   } }
 
   scope :order_alphabetically_by_non_default_role, lambda { |order| {
     :select => "ministry_person.*",
     :joins => "JOIN organizational_roles ON ministry_person.personID = organizational_roles.person_id JOIN roles ON organizational_roles.role_id = roles.id",
-    :order => "FIELD #{Role.i18n_field_plus_default_roles_for_field_string(order.include?("asc") ? Role::DEFAULT_ROLES : Role::DEFAULT_ROLES.reverse)}"
+    :conditions => "roles.name NOT IN #{Role.default_roles_for_field_string(order.include?("asc") ? Role::DEFAULT_ROLES : Role::DEFAULT_ROLES.reverse)}",
+    :order => "roles.i18n #{order.include?("asc") ? 'DESC' : 'ASC'}"
   } }
 
   def update_date_attributes_updated
