afcc99495956722f214b6aff886ba2be6b7b295d
neilmarion <neilmarion@neilmarion-notebook.(none)>
2012-03-20 22:45:53 +0800

    Fixed PeopleController update_roles bug

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -342,16 +342,20 @@ class PeopleController < ApplicationController
     #The purpose of this code block is to avoid emailing (that a Person has just become a leader) a Person if he is already a leader before this roles updateh
     organizational_role_ids = organizational_role_ids - role_ids
     organizational_roles = person.organizational_roles.where(organization_id: current_organization.id).collect { |role| role.id if organizational_role_ids.include?(role.role_id.to_s) }
-    OrganizationalRole.delete(organizational_roles)    
+    OrganizationalRole.delete(organizational_roles)
+
+    # place admin and leader role ids at the end of the array
+    role_ids.insert(role_ids.length-1, role_ids.delete_at(0)) if role_ids[0] == Role::ADMIN_ID
+    role_ids.insert(role_ids.length-1, role_ids.delete_at(0)) if role_ids[0] == Role::LEADER_ID
 
     role_ids.uniq.each_with_index do |role_id, index|
         begin
           begin       
             OrganizationalRole.find_or_create_by_person_id_and_organization_id_and_role_id(person_id: person.id, role_id: role_id, organization_id: current_organization.id, added_by_id: current_user.person.id) 
           rescue OrganizationalRole::InvalidPersonAttributesError
-          render 'update_leader_error', :locals => { :person => person } if role_id == Role::LEADER_ID
-          render 'update_admin_error', :locals => { :person => person } if role_id == Role::ADMIN_ID
-          return
+            render 'update_leader_error', :locals => { :person => person } if role_id == Role::LEADER_ID
+            render 'update_admin_error', :locals => { :person => person } if role_id == Role::ADMIN_ID
+            return
         rescue ActiveRecord::RecordNotUnique
         end
       end
