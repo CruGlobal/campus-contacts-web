f1d691bce9dc8f639023cbc56e0ddf976dca7a37
neilmarion <neilmarion@neilmarion-notebook.(none)>
2012-03-31 02:10:32 +0800

    Added tests

--- test/functional/admin/organizations_controller_test.rb
+++ test/functional/admin/organizations_controller_test.rb
@@ -7,7 +7,7 @@ class Admin::OrganizationsControllerTest < ActionController::TestCase
       org_child = Factory(:organization, :name => "neilmarion", :parent => org1)
     end
 
-    should "not create organization if it's name is equal to one of its sibling" do
+    should "not create organization if its name is equal to one of its sibling" do
       post :update, {:organization => {:parent_id =>"23", :name => "Bandana", :terminology => "Organization", :show_sub_orgs => "1"}}
       assert_equal Organization.count, 2, "Created organization with the same name as its sibling."
       assert_response :success
