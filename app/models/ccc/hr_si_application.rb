992abd62289cac8ce603978849bbc8437cac043e
Josh Starcher <josh.starcher@gmail.com>
2012-05-23 17:16:02 -0400

    add some si relationships

--- app/models/ccc/hr_si_application.rb
+++ app/models/ccc/hr_si_application.rb
@@ -1,5 +1,6 @@
 class Ccc::HrSiApplication < ActiveRecord::Base
-	# belongs_to :person
   self.primary_key = 'applicationID'
-  
+  has_one :tracking, class_name: 'Ccc::SitrackTracking', foreign_key: :application_id, dependent: :destroy
+  belongs_to :person, foreign_key: :fk_personID
+  belongs_to :si_apply, class_name: 'Ccc::SiApply', foreign_key: :apply_id
 end
