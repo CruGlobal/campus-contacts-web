6f7b4a58e0b1c8f97889ec8a3ba5d5b41f9ad5cd
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2012-06-01 01:07:04 +0800

    Working revisions

--- lib/contact_actions.rb
+++ lib/contact_actions.rb
@@ -63,14 +63,19 @@ module ContactActions
   def save_survey_answers
     @answer_sheets = []
     @organization ||= current_organization
-
-    @organization.surveys.each do |survey|
-      @answer_sheet = get_answer_sheet(survey, @person)
-      question_set = QuestionSet.new(survey.questions, @answer_sheet)
-      question_set.post(params[:answers], @answer_sheet)
-      question_set.save
-      @answer_sheets << @answer_sheet
+    
+    params[:answers].each do |survey|
+      survey_id = survey[0]
+      fields = survey[1]
+      if survey = @organization.surveys.find(survey_id)
+        @answer_sheet = get_answer_sheet(survey, @person)
+        question_set = QuestionSet.new(survey.questions, @answer_sheet)
+        question_set.post(fields, @answer_sheet)
+        question_set.save
+        @answer_sheets << @answer_sheet
+      end
     end
+    
     # Delete any answer_sheet with no answers
     @answer_sheets.each do |as|
       if as.reload.answers.blank?
