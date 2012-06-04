82bb86494e17d1d6abae6bf879af700025f51175
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2012-06-05 00:06:17 +0800

    Ready to delete answer_sheets & answers

--- app/controllers/organizational_roles_controller.rb
+++ app/controllers/organizational_roles_controller.rb
@@ -3,9 +3,24 @@ class OrganizationalRolesController < ApplicationController
     @organizational_role = OrganizationalRole.find(params[:id])
     @organizational_role.followup_status = params[:status]
     if params[:status] == 'do_not_contact'
-      ContactAssignment.where(person_id: @organizational_role.person_id, organization_id: @organizational_role.organization_id).destroy_all
+      person_id = @organizational_role.person_id
+      organization_id = @organizational_role.organization_id
+      
+      contact_assignments = ContactAssignment.where(person_id: person_id, organization_id: organization_id)
+      survey_ids = Survey.where(organization_id: organization_id).collect{|s| s.id}
+      answer_sheets = AnswerSheet.where(survey_id: survey_ids, person_id: person_id)
+      answer_sheets_ids = answer_sheets.collect{|a| a.id}
+      answers = Answer.where(answer_sheet_id: answer_sheets_ids)
+      
+      
+      Rails.logger.info ""
+      Rails.logger.info "Delete #{contact_assignments.count} Contact Assignments"
+      Rails.logger.info "Delete #{answer_sheets.count} Answer Sheets"
+      Rails.logger.info "Delete #{answers.count} Answers"
+      Rails.logger.info ""
+      
     end
-    @organizational_role.save
+    # @organizational_role.save
     respond_to do |wants|
       wants.html
       wants.js {render nothing: true}
