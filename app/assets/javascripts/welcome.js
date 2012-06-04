e74f7c2836b2e758aeac6cbc0b7d765f2775d70a
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2012-06-04 22:03:16 +0800

    Clean welcome.js

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -6,42 +6,33 @@ jQuery(document).ready(function(){
 	// Show first video as default
 	select_video_by_position(0);
 	
+	// Make videos in the list a link
 	$("#welcome_controller .inner_space .menu .list .video_entry").click(function(){
-		select_video_by_id($(this).attr('id'));
+		select_video_by_id($(this));
 	});
+	
 })
 
-
 function show_video_preview(){
-	videos = $("#welcome_controller .inner_space .menu .list .video_entry");
-	for(i=0; i<videos.length; i++){
-		video = videos.eq(i);
-		preview = video.children('.preview');
-		id = video.children('.youtube_video_id').html();
-		video.attr('id',id);
-		preview.html("<img src='http://img.youtube.com/vi/"+id+"/3.jpg'>");
-	}
+	$("#welcome_controller .video_entry").each(function(){
+		id = $(this).children('.youtube_video_id').html();
+		$(this).children('.preview').html("<img src='http://img.youtube.com/vi/"+id+"/3.jpg'>");
+	});
 }
 
-function select_video_by_id(video_id){ 
-	video = $("#"+video_id+".video_entry");
-	id = video.children('.youtube_video_id').html();
-	frame = $("#video_frame");
-	video_url = "http://www.youtube.com/embed/"+id+"?showinfo=0&autohide=1";
-	frame.attr("src", video_url);
-	description_space = $("#welcome_controller .inner_space .content .description");
-	description_space.children('.title').html(video.children('.title').html());
-	description_space.children('.details').html(video.children('.description').html());
+function select_video_by_id(video_div){ 
+	id = video_div.children('.youtube_video_id').html();
+	info = $("#welcome_controller .inner_space .content .description");
+	info.children('.title').html(video_div.children('.title').html());
+	info.children('.details').html(video_div.children('.description').html());
+	$("#video_frame").attr("src", "http://www.youtube.com/embed/"+id+"?showinfo=0&autohide=1");
 }
 
 function select_video_by_position(video_position){
-	videos = $("#welcome_controller .inner_space .menu .list .video_entry");
-	video = videos.eq(video_position);
+	video = $("#welcome_controller .video_entry").eq(video_position);
 	id = video.children('.youtube_video_id').html();
-	frame = $("#video_frame");
-	video_url = "http://www.youtube.com/embed/"+id+"?showinfo=0&autohide=1";
-	frame.attr("src", video_url);
-	description_space = $("#welcome_controller .inner_space .content .description");
-	description_space.children('.title').html(video.children('.title').html());
-	description_space.children('.details').html(video.children('.description').html());
+	info = $("#welcome_controller .inner_space .content .description");
+	info.children('.title').html(video.children('.title').html());
+	info.children('.details').html(video.children('.description').html());
+	$("#video_frame").attr("src", "http://www.youtube.com/embed/"+id+"?showinfo=0&autohide=1");
 }
