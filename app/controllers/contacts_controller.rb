cf642a1e8bd7a3024a3fe853fdaae4410ff0af70
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2012-06-16 05:01:37 +0800

    reverted to updated_at

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -317,7 +317,7 @@ class ContactsController < ApplicationController
         
         answers[answer_sheet.person_id] ||= {}
         questions.each do |q|
-          answers[answer_sheet.person_id][q.id] = [q.display_response(answer_sheet), answer_sheet.created_at] if q.display_response(answer_sheet).present?
+          answers[answer_sheet.person_id][q.id] = [q.display_response(answer_sheet), answer_sheet.updated_at] if q.display_response(answer_sheet).present?
         end
       end
       answers
