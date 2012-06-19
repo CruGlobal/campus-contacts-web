e54610b3bfa683632e579f66c05e070d4228bd9b
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2012-06-19 17:49:50 +0800

    Add tutorial page

--- config/routes.rb
+++ config/routes.rb
@@ -165,6 +165,7 @@ Mh::Application.routes.draw do
   end
 
   get "welcome/index"
+  get "welcome/tutorials"
   get "/test" => "welcome#test"
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", sessions: "sessions" }
