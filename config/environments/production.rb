1d902106d644444f1d0ad3324b69cef859cdb8ac
Josh Starcher <josh.starcher@gmail.com>
2012-06-20 02:15:32 -0400

    explicitly use sendmail in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,8 @@ Mh::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.delivery_method = :sendmail
+
   config.action_mailer.default_url_options = {
     host: 'missionhub.com'
   }
