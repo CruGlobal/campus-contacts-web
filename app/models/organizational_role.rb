306d750bb8ca7fd73b863eab3377f92cc5e6808e
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2012-07-20 22:49:44 +0800

    Fixed '==' operator and changed the en.yml

--- app/models/organizational_role.rb
+++ app/models/organizational_role.rb
@@ -88,7 +88,7 @@ class OrganizationalRole < ActiveRecord::Base
     end
     
     def check_if_admin_is_destroying_own_admin_role
-      raise CannotDestroyOwnAdminRoleError if destroyer && person_id = destroyer.id
+      raise CannotDestroyOwnAdminRoleError if destroyer && person_id == destroyer.id
     end
   
     def set_start_date
