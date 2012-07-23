b4bc62d834f31e39d9c4fee95315765940ae09ad
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2012-07-23 08:39:56 +0800

    Fixed searching through email

--- app/models/person.rb
+++ app/models/person.rb
@@ -96,7 +96,7 @@ class Person < ActiveRecord::Base
   scope :search_by_name_or_email, lambda { |keyword, org_id| {
     :select => "ministry_person.*",
     :joins => "LEFT JOIN email_addresses AS emails ON emails.person_id = ministry_person.personID LEFT JOIN organizational_roles AS org_roles ON ministry_person.personID = org_roles.person_id",
-    :conditions => "org_roles.organization_id = #{org_id} AND org_roles.deleted <> 1 AND concat(firstName,' ',lastName) LIKE '%#{keyword}%' OR concat(lastName, ' ',firstName) LIKE '%#{keyword}%' OR emails.email LIKE '%#{keyword}%'"
+    :conditions => "(org_roles.organization_id = #{org_id} AND concat(firstName,' ',lastName) LIKE '%#{keyword}%' OR concat(lastName, ' ',firstName) LIKE '%#{keyword}%' OR emails.email LIKE '%#{keyword}%') AND org_roles.deleted <> 1"
   } }
   
   scope :get_and_order_by_latest_answer_sheet_answered, lambda { |order, org_id| {
