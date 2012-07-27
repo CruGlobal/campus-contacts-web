886b3fbcf41e3601b1c35cb2bed9f0f5d68139f3
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2012-07-27 13:41:25 +0800

    Small mistake on retrieving archived people fixed

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -561,7 +561,7 @@ class PeopleController < ApplicationController
     end
 
     @all_people = @all_people.where(personID: params[:ids].split(',')) if params[:custom]    
-    @all_people = @all_people.where(personID: current_organization.archived(current_organization.id).uniq.collect(&:personID)) unless params[:archived].blank?
+    @all_people = @all_people.where(personID: current_organization.people.archived(current_organization.id).uniq.collect(&:personID)) unless params[:archived].blank?
     #Person.archived_not_included query must be fixed so that we don't have to query from db twice such as the line above
     @all_people = @all_people.where(personID: current_organization.people.archived.where("organizational_roles.archive_date > ? AND organizational_roles.archive_date < ?", params[:archived_date], (params[:archived_date].to_date+1).strftime("%Y-%m-%d")).collect{|x| x.personID}) unless params[:archived_date].blank?
     #Person.archived_not_included query must be fixed so that we don't have to query from db twice such as the line above
