8ccca7639bb85c973d0c61bc8d854f09b277aac5
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2012-08-07 23:20:15 +0800

    Fixed the NoMethodError: undefined method 'split' for nil:NilClass error on surveys/questions#create

--- test/functional/surveys/questions_controller_test.rb
+++ test/functional/surveys/questions_controller_test.rb
@@ -6,30 +6,46 @@ class Surveys::QuestionsControllerTest < ActionController::TestCase
     sign_in @user
     Factory(:approved_keyword, organization: @org, user: @user)
     @survey = @org.surveys.first
+    @predefined_survey = Factory(:survey, organization: @org)
+    APP_CONFIG['predefined_survey'] = @predefined_survey.id
   end
   
-  test "should get index" do
-    APP_CONFIG['predefined_survey'] = Factory(:survey).id
-    get :index, survey_id: @survey.id 
-    assert_response :success
-  end
+  #test "should get index" do
+  #  APP_CONFIG['predefined_survey'] = Factory(:survey).id
+  #  get :index, survey_id: @survey.id 
+  #  assert_response :success
+  #end
 
-  context "When updating a question" do
+  #context "When updating a question" do
+  #  setup do
+  #    @leader1 = Factory(:user_with_auxs)
+  #   @leader2 = Factory(:user_with_auxs)
+  #   @leader3 = Factory(:user_with_auxs)
+  #    @survey = Factory(:survey, organization: x.organization)
+  #    @element = Factory(:text_field, label: 'foobar')
+  #    @survey_element = Factory(:survey_element, survey: survey, element: element, position: 1, archived: true)
+  #  end
+
+    #should "update to-be-notified-leaders" do
+    #  xhr :post, :update, {:text_field =>{:label => @element.label, :content => "", :trigger_words => "Hello", :notify_via => "Email", :web_only => "0"}, :leaders => [@leader1.id], :commit => "Update", :survey_id => @survey.id, :id => @survey_element.id}
+    #  assert_response :success
+    #end
+  #end
+  
+  context "When updating questions" do
     setup do
-      @leader1 = Factory(:user_with_auxs)
-      @leader2 = Factory(:user_with_auxs)
-      @leader3 = Factory(:user_with_auxs)
-      @survey = Factory(:survey, organization: x.organization)
-      @element = Factory(:text_field, label: 'foobar')
-      @survey_element = Factory(:survey_element, survey: survey, element: element, position: 1, archived: true)
+      element = Factory(:choice_field, label: 'foobar')
+      @q1 = Factory(:survey_element, survey: @org.surveys.first, element: element, position: 1, archived: true)
+      element = Factory(:choice_field)
+      @q2 = Factory(:survey_element, survey: @org.surveys.first, element: element, position: 2)
     end
-
-=begin
-    should "update to-be-notified-leaders" do
-      xhr :post, :update, {:text_field =>{:label => @element.label, :content => "", :trigger_words => "Hello", :notify_via => "Email", :web_only => "0"}, :leaders => [@leader1.id], :commit => "Update", :survey_id => @survey.id, :id => @survey_element.id}
+    
+    should "be able to select a predefined or previously used question" do
+      puts @org.surveys.inspect
+    
+      xhr :post, :create, { :question_id => @q1.id, :survey_id => @org.surveys.first.id}
       assert_response :success
     end
-=end
   end
   
 end
