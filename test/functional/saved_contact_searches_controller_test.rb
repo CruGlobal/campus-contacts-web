14718b74649cfae4999698d6f6eb4121033ada15
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2012-08-16 11:57:09 +0800

    covered saved_contact_searches_controller_test

--- test/functional/saved_contact_searches_controller_test.rb
+++ test/functional/saved_contact_searches_controller_test.rb
@@ -17,18 +17,39 @@ class SavedContactSearchesControllerTest < ActionController::TestCase
     assert_equal 1, SavedContactSearch.count
   end
   
-  test "update" do
-    post :create, { :saved_contact_search => { :name => "Test", :full_path => "localhost:3000", :user_id => @user.id } }
-    assert_response :redirect
-    assert_equal 1, SavedContactSearch.count
+  context "updating a saved_contact_search" do
+  
+    should "update" do
+      post :create, { :saved_contact_search => { :name => "Test", :full_path => "localhost:3000", :user_id => @user.id } }
+      assert_response :redirect
+      assert_equal 1, SavedContactSearch.count
+      
+      s = SavedContactSearch.last
+      
+      post :update, { :id => s.id, :saved_contact_search => { :name => "Wat", :full_path => "localhost:3000" } }
+      assert_response :redirect
+      
+      assert_equal "Wat", SavedContactSearch.last.name
+    end
     
-    s = SavedContactSearch.last
-    
-    post :update, { :id => s.id, :saved_contact_search => { :name => "Wat", :full_path => "localhost:3000" } }
-    assert_response :redirect
-    
-    assert_equal "Wat", SavedContactSearch.last.name
+    should "still be able to update (create) if a person deleted his search and clicked update" do
+      post :create, { :saved_contact_search => { :name => "Test", :full_path => "localhost:3000", :user_id => @user.id } }
+      assert_response :redirect
+      assert_equal 1, SavedContactSearch.count
+      
+      s = SavedContactSearch.last
+      
+      xhr :post, :destroy, { :id => s.id }
+      assert_equal " ", response.body
+      assert_equal 0, SavedContactSearch.count
+      
+      post :update, { :id => s.id, :saved_contact_search => { :name => "Wat", :full_path => "localhost:3000" } }
+      assert_response :redirect
+      
+      assert_equal "Wat", SavedContactSearch.last.name
+    end
   end
+  
   
   test "destroy" do
     post :create, { :saved_contact_search => { :name => "Test", :full_path => "localhost:3000", :user_id => @user.id } }
