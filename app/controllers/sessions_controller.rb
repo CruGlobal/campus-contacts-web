c3cbf7c5ab1be7ca9d2bd7ce867812f8da0bfcf4
Josh Starcher <josh.starcher@gmail.com>
2012-08-20 15:12:05 -0400

    another attempt at cleaning up post-logout url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,7 +27,14 @@ class SessionsController < Devise::SessionsController
   protected
   
   def after_sign_out_path_for(resource_or_scope)
-    params[:next] ? params[:next] : user_root_path
+    case
+    when params[:next]
+      params[:next]
+    when current_user
+      user_root_path
+    else
+      root_path
+    end
   end
   
   def pick_layout
