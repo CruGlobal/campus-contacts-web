db549fc8d2d60ea0db6769e99d3a5299ab0e6fd0
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2012-09-01 09:10:40 +0800

    removed comment

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -511,7 +511,6 @@ class PeopleController < ApplicationController
     org_ids = params[:subs] == 'true' ? current_organization.self_and_children_ids : current_organization.id
     @people_scope = Person.where('organizational_roles.organization_id' => org_ids).includes(:organizational_roles_including_archived)
     @people_scope = @people_scope.where(personID: @people_scope.archived_not_included.collect(&:personID)) if params[:include_archived].blank? && params[:archived].blank?
-    #Person.archived_not_included query must be fixed so that we don't have to query from db twice such as the line above
     
     @q = @people_scope.includes(:primary_phone_number, :primary_email_address)
     #when specific role is selected from the directory
