9501de8432f71edd43c35a0d27f21ecf74e137e8
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2012-09-03 13:19:03 +0800

    Removed ReferenceSheetController bec it seems it has no use.

--- config/routes.rb
+++ config/routes.rb
@@ -103,33 +103,7 @@ Mh::Application.routes.draw do
 
   namespace :admin do
     resources :email_templates
-    resources :question_sheets do
-      member do
-        post :archive
-        post :unarchive
-        post :duplicate
-      end
-      resources :pages,                               # pages/
-                controller: :question_pages do         # question_sheet_pages_path(),
-                collection do
-                  post :reorder
-                end
-                member do
-                  get :show_panel
-                end
-        resources :elements do
-                  collection do
-                    post :reorder
-                  end
-                  member do
-                    get :remove_from_grid
-                    post :use_existing
-                    post :drop
-                    post :duplicate
-                  end
-                end
-      end
-    end
+    
   end
 
   # namespace :admin do
