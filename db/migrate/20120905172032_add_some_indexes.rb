e595e322ba05dc8dc191b2b125241f74a7c4c548
Josh Starcher <josh.starcher@gmail.com>
2012-09-05 13:38:03 -0400

    fix migration

--- db/migrate/20120905172032_add_some_indexes.rb
+++ db/migrate/20120905172032_add_some_indexes.rb
@@ -1,7 +1,7 @@
 class AddSomeIndexes < ActiveRecord::Migration
   def change
     add_index :ministry_person, [:firstName, :lastName], name: 'firstName_lastName'
-    remove_index :ministry_person, 'firstname_ministry_Person'
+    remove_index :ministry_person, name: 'firstname_ministry_Person'
     add_index :phone_numbers, :number
     add_index :mh_survey_elements, [:survey_id, :element_id], name: 'survey_id_element_id'
     add_index :mh_answer_sheets, [:person_id, :survey_id], name: 'person_id_survey_id'
