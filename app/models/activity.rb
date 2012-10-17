3ead6628bbc973506b26a1de44ff9c5a68d7249c
Josh Starcher <josh.starcher@gmail.com>
2012-10-17 09:01:50 -0400

    fix link from organization to target area

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,13 +1,12 @@
 class Activity < ActiveRecord::Base
-  establish_connection :uscm
 
-  belongs_to :target_area
+  belongs_to :target_area, class_name: 'Ccc::TargetArea'
   belongs_to :organization
 
   before_validation :set_start_date, on: :create
-  
+
   protected
-  
+
   def set_start_date
     self.start_date = Date.today
   end
