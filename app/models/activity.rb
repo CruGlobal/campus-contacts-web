b9ebf3c2df340b2eeb3b336ef752c9a71ff5812f
Josh Starcher <josh.starcher@gmail.com>
2012-10-18 09:12:45 -0400

    fix class name

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,6 @@
 class Activity < ActiveRecord::Base
 
-  belongs_to :target_area, class_name: 'Ccc::TargetArea'
+  belongs_to :target_area
   belongs_to :organization
 
   before_validation :set_start_date, on: :create
