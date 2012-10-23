f9d378efaaa1c57882daad8136d5371fe49aac7d
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2012-10-24 01:03:05 +0800

    More fixes for Label Search in All Contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -190,28 +190,32 @@ class ContactsController < ApplicationController
       elsif params[:archived].present? && params[:archived] == 'true'
         @header = I18n.t('contacts.index.archived')
         @people = Person.where(id: current_organization.people.archived(current_organization.id).collect(&:id))
-      elsif params[:search] || (params[:role] && Role.exists?(id: params[:role]))
-        @role = Role.find(params[:role])
-        @people = @people_scope.where('organizational_roles.role_id = ? AND organizational_roles.organization_id = ? AND organizational_roles.deleted = 0', @role.id, current_organization.id)
-        if params[:include_archived].present? && params[:include_archived] == 'true'
-          @people = @people.select("people.*, roles.*")
-                    .joins("LEFT JOIN organizational_roles AS org_roles ON 
-                      org_roles.person_id = people.id")
-                    .joins("INNER JOIN roles ON roles.id = org_roles.role_id")
-                    .where("org_roles.organization_id" => current_organization.id)
-                    .where("roles.id = #{@role.id}")
-                    sort_by.unshift("roles.id").uniq
-        else
-          @people = @people.select("people.*, roles.*")
-                    .joins("LEFT JOIN organizational_roles AS org_roles ON 
-                      org_roles.person_id = people.id")
-                    .joins("INNER JOIN roles ON roles.id = org_roles.role_id")
-                    .where("org_roles.archive_date" => nil, "org_roles.organization_id" => current_organization.id) 
-                    .where("roles.id = #{@role.id}")
-                    sort_by.unshift("roles.id").uniq
+      elsif params[:role] || (params[:search] && params[:role])
+        if @role = Role.find(params[:role])
+          @people = @people_scope.where('organizational_roles.role_id = ? AND organizational_roles.organization_id = ? AND organizational_roles.deleted = 0', @role.id, current_organization.id)
+          if params[:include_archived].present? && params[:include_archived] == 'true'
+            @people = @people.select("people.*, roles.*")
+                      .joins("LEFT JOIN organizational_roles AS org_roles ON 
+                        org_roles.person_id = people.id")
+                      .joins("INNER JOIN roles ON roles.id = org_roles.role_id")
+                      .where("org_roles.organization_id" => current_organization.id)
+                      .where("roles.id = #{@role.id}")
+                      sort_by.unshift("roles.id").uniq
+          else
+            @people = @people.select("people.*, roles.*")
+                      .joins("LEFT JOIN organizational_roles AS org_roles ON 
+                        org_roles.person_id = people.id")
+                      .joins("INNER JOIN roles ON roles.id = org_roles.role_id")
+                      .where("org_roles.archive_date" => nil, "org_roles.organization_id" => current_organization.id) 
+                      .where("roles.id = #{@role.id}")
+                      sort_by.unshift("roles.id").uniq
                     
+          end
+          @header = params[:search] ? I18n.t('contacts.index.matching_seach') : @role.i18n
         end
-        @header = params[:search] ? I18n.t('contacts.index.matching_seach') : @role.i18n
+      elsif params[:search]
+        @header = I18n.t('contacts.index.matching_seach')
+        @people = @people_scope
       else
         params[:assigned_to] = nil if params[:assigned_to].blank?
         if params[:assigned_to]
