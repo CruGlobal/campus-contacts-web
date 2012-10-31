1db2aae38307d6a09461b09591f411cfe74d135f
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2012-11-01 01:32:53 +0800

    Refactor label search

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -189,19 +189,14 @@ class ContactsController < ApplicationController
         if @role = Role.find(params[:role])
           @people = @people_scope.where('organizational_roles.role_id = ? AND organizational_roles.organization_id = ? AND organizational_roles.deleted = 0', @role.id, current_organization.id)
           if params[:include_archived].present? && params[:include_archived] == 'true'
-            @people = @people.select("people.*, roles.*")
-                      .joins("LEFT JOIN organizational_roles AS org_roles ON 
-                        org_roles.person_id = people.id")
-                      .joins("INNER JOIN roles ON roles.id = org_roles.role_id")
-                      .where("org_roles.organization_id" => current_organization.id)
+            @people = @people
+                      .joins("INNER JOIN roles ON roles.id = organizational_roles.role_id")
                       .where("roles.id = #{@role.id}")
                       sort_by.unshift("roles.id").uniq
           else
-            @people = @people.select("people.*, roles.*")
-                      .joins("LEFT JOIN organizational_roles AS org_roles ON 
-                        org_roles.person_id = people.id")
-                      .joins("INNER JOIN roles ON roles.id = org_roles.role_id")
-                      .where("org_roles.archive_date" => nil, "org_roles.organization_id" => current_organization.id) 
+            @people = @people
+                      .joins("INNER JOIN roles ON roles.id = organizational_roles.role_id")
+                      .where("organizational_roles.archive_date" => nil) 
                       .where("roles.id = #{@role.id}")
                       sort_by.unshift("roles.id").uniq
                     
