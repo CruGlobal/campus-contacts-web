64b68559b98e30b90e1af382da70f670ce5bb11f
Josh Starcher <josh.starcher@gmail.com>
2012-11-16 16:28:21 -0500

    prefill more gift values

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  include ActionView::Helpers::DateHelper  
+  include ActionView::Helpers::DateHelper
 
   def uri?(string)
     uri = URI.parse(string)
@@ -11,12 +11,12 @@ module ApplicationHelper
   def tip(tip)
     "<span class=\"tiplight\" title=\"#{h(tip)}\" style=\"float: none;\"></span>".html_safe
   end
-  
+
   def spinner(extra = nil)
     e = extra ? "spinner_#{extra}" : 'spinner'
     image_tag('spinner.gif', id: e, style: 'display:none', class: 'spinner')
   end
-  
+
   def add_params(url, hash)
     if url.is_a?(Hash)
       url.merge(hash)
@@ -48,8 +48,22 @@ module ApplicationHelper
   end
 
   def give_link(text)
-    if text.include?('https://give.cru.org/') && text.include?('Desig')
-      text.sub('Desig', "id_type=missionhub&id_value=#{current_person.id}&Desig")
+    if text.include?('Desig')
+      extra_params = { 'Prefix' => current_person.gender == 'Male' ? 'Mr' : 'Ms',
+                       'FirstName' => current_person.first_name,
+                       'LastName' => current_person.last_name,
+                       'id_type' => 'missionhub',
+                       'id_value' => current_person.id }
+      if a = current_person.current_address
+        extra_params.merge!({ 'StreetLine1' => a.address1,
+                              'StreetLine2' => a.address2,
+                              'StreetLine3' => a.address3,
+                              'City' => a.city,
+                              'State' => a.state,
+                              'PostalCode' => a.zip,
+                              'Country' => a.country })
+      end
+      text.sub('Desig', extra_params.collect {|k, v| "#{k}=#{v}"}.join('&') + '&Desig')
     else
       text
     end.html_safe
@@ -74,19 +88,19 @@ module ApplicationHelper
     #end
     #ret
   #end
-  
+
   def site_name
     mhub? ? '' : 'MissionHub'
   end
-  
+
   def site_email
     mhub? ? 'support@mhub.cc' : 'support@missionhub.com'
   end
-  
+
   def link_to_remove_fields(f, hidden)
     f.hidden_field(:_destroy) + link_to(' ', '#', class: 'remove_field', style: hidden ? 'display:none' : '')
   end
-  
+
   def link_to_add_fields(name, f, association)
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
@@ -94,7 +108,7 @@ module ApplicationHelper
     end
     link_to_function(name, raw("addFields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"))
   end
-  
+
   def times(start_time, end_time)
     midnight = Time.now.beginning_of_day
     # start_time = midnight + start_time.hours
@@ -105,8 +119,8 @@ module ApplicationHelper
       time_options << [l(midnight + time, format: :time_only), time]
     end
     time_options
-  end  
-  
+  end
+
   def no_left_sidebar
     case "#{params[:controller]}/#{params[:action]}"
     when 'groups/create', 'groups/update', 'organizations/index', 'organizations/edit', 'organizations/update',
@@ -116,11 +130,11 @@ module ApplicationHelper
       false
     end
   end
-  
+
   def declare_ownership(name)
     name.slice(-1, 1) == 's' ? "#{name}'" : "#{name}'s"
   end
-  
+
   def pretty_tag(txt)
     txt.to_s.gsub(/\s/, "_").gsub(/(?!-)\W/, "").downcase
   end
@@ -138,19 +152,19 @@ module ApplicationHelper
         flat_hash[key] = v
       end
     end
-    
+
     flat_hash
   end
 
   def flat_hash_key(names)
     names = Array.new(names)
-    name = names.shift.to_s.dup 
+    name = names.shift.to_s.dup
     names.each do |n|
       name << "[#{n}]"
     end
     name
   end
-  
+
   def questionnaire_engine_stylesheets(options = {})
     output = []
     output << "qe/qe.screen"
@@ -171,23 +185,23 @@ module ApplicationHelper
     end
     output << "qe/jquery.scrollTo-min"
     output << {:cache => true} if options[:cache]
-    return output 
+    return output
   end
 
   def questionnaire_engine_includes(options = {})
     return "" if @qe_already_included
     @qe_already_included=true
-    
+
     js = questionnaire_engine_javascripts(options).collect {|file| javascript_include_tag(file)}.join("\n")
     css = questionnaire_engine_stylesheets(options).collect {|file| stylesheet_link_tag(file)}.join("\n")
     "#{js}\n#{css}\n".html_safe
   end
-  
+
   def calendar_date_select_tag(name, value = nil, options = {})
     options.merge!({'data-calendar' => true})
     text_field_tag(name, value, options )
   end
-  
+
   def tip(t)
     image_tag('qe/icons/question-balloon.png', :title => t, :class => 'tip')
   end
