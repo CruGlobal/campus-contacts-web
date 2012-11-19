7f028bb93c4ae47e505cdabca000b363aaf9f9d8
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2012-11-19 16:46:58 +0800

    Update Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121103161704) do
+ActiveRecord::Schema.define(:version => 20121109101429) do
 
   create_table "access_grants", :force => true do |t|
     t.string   "code"
@@ -363,6 +363,7 @@ ActiveRecord::Schema.define(:version => 20121103161704) do
     t.datetime "upload_updated_at"
     t.text     "headers"
     t.text     "header_mappings"
+    t.text     "preview"
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
   end
