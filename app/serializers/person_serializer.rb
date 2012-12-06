5876a5660b8c5e15be1798593bef92fbc399d5eb
Josh Starcher <josh.starcher@gmail.com>
2012-12-06 11:10:45 -0500

    Add contact assignments to the api

--- app/serializers/person_serializer.rb
+++ app/serializers/person_serializer.rb
@@ -1,18 +1,24 @@
 class PersonSerializer < ActiveModel::Serializer
-  INCLUDES = [:phone_numbers, :email_addresses, :person_transfers, :contact_assignments,
-           :followup_comments, :organizational_roles, :rejoicables, :user, :answer_sheets]
+  HAS_MANY = [:phone_numbers, :email_addresses, :person_transfers, :contact_assignments,
+             :followup_comments, :organizational_roles, :rejoicables, :answer_sheets]
+
+  HAS_ONE = [:user]
+
+  INCLUDES = HAS_MANY + HAS_ONE
 
   attributes :id, :first_name, :last_name, :gender, :campus, :year_in_school, :major, :minor, :birth_date,
              :date_became_christian, :graduation_date, :user_id, :fb_uid, :created_at, :updated_at
 
-  has_many *INCLUDES
-  has_one :user
+  has_many *HAS_MANY
+  has_one *HAS_ONE
 
   def include_associations!
     includes = scope if scope.is_a? Array
     includes = scope[:include] if scope.is_a? Hash
     includes.each do |rel|
-      include!(rel.to_sym) if INCLUDES.include?(rel.to_sym)
+      if INCLUDES.include?(rel.to_sym)
+        include!(rel.to_sym)
+      end
     end if includes
   end
 
@@ -31,7 +37,7 @@ class PersonSerializer < ActiveModel::Serializer
   def rejoicables
     add_since(organization_filter(:rejoicables))
   end
-  
+
   [:phone_numbers, :email_addresses, :person_transfers, :user, :answer_sheets].each do |relationship|
     define_method(relationship) do
       add_since(object.send(relationship))
