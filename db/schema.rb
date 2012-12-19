a48d235005b6936ad1faa57cc3668c5718a3316e
Marvin Punongbayan <marvindpunongbayan@gmail.com>
2012-12-19 19:49:23 +0800

    Add pagination to the top and bottom for contacts page

--- db/schema.rb
+++ db/schema.rb
@@ -280,7 +280,7 @@ ActiveRecord::Schema.define(:version => 20121205205752) do
     t.datetime "updated_at"
   end
 
-  add_index "email_addresses", ["email"], :name => "email"
+  add_index "email_addresses", ["email"], :name => "index_email_addresses_on_email"
   add_index "email_addresses", ["person_id"], :name => "person_id"
 
   create_table "followup_comments", :force => true do |t|
@@ -295,6 +295,17 @@ ActiveRecord::Schema.define(:version => 20121205205752) do
   end
 
   add_index "followup_comments", ["organization_id", "contact_id"], :name => "comment_organization_id_contact_id"
+
+  create_table "friends_deprecated", :force => true do |t|
+    t.string   "name"
+    t.string   "uid"
+    t.string   "provider"
+    t.integer  "person_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "friends_deprecated", ["person_id", "uid"], :name => "person_uid", :unique => true
 
   create_table "group_labelings", :force => true do |t|
     t.integer  "group_id"
@@ -456,10 +467,10 @@ ActiveRecord::Schema.define(:version => 20121205205752) do
     t.string   "first_name",                    :limit => 50
     t.string   "middle_name",                   :limit => 50
     t.string   "gender",                        :limit => 1
+    t.string   "campus",                        :limit => 128
     t.string   "year_in_school",                :limit => 20
     t.string   "major",                         :limit => 70
     t.string   "minor",                         :limit => 70
-    t.string   "campus",                        :limit => 70
     t.string   "greek_affiliation",             :limit => 50
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -480,6 +491,7 @@ ActiveRecord::Schema.define(:version => 20121205205752) do
   end
 
   add_index "people", ["accountNo"], :name => "accountNo_ministry_Person"
+  add_index "people", ["campus"], :name => "campus"
   add_index "people", ["crs_profile_id"], :name => "index_people_on_crs_profile_id"
   add_index "people", ["fb_uid"], :name => "index_ministry_person_on_fb_uid"
   add_index "people", ["first_name", "last_name"], :name => "firstName_lastName"
@@ -640,6 +652,13 @@ ActiveRecord::Schema.define(:version => 20121205205752) do
 
   add_index "saved_contact_searches", ["user_id"], :name => "index_saved_contact_searches_on_user_id"
 
+  create_table "sent_people", :force => true do |t|
+    t.integer  "person_id"
+    t.integer  "transferred_by_id"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+  end
+
   create_table "sent_sms", :force => true do |t|
     t.text     "message"
     t.string   "recipient"
@@ -684,7 +703,7 @@ ActiveRecord::Schema.define(:version => 20121205205752) do
     t.string   "initial_response",               :limit => 145
     t.text     "post_survey_message_deprecated"
     t.string   "event_type"
-    t.string   "gateway",                                       :default => "twilio", :null => false
+    t.string   "gateway",                                       :default => "", :null => false
     t.integer  "survey_id"
   end
 
@@ -793,13 +812,9 @@ ActiveRecord::Schema.define(:version => 20121205205752) do
   add_index "versions", ["organization_id", "created_at"], :name => "index_versions_on_organization_id_and_created_at"
   add_index "versions", ["person_id", "created_at"], :name => "index_versions_on_person_id_and_created_at"
 
-  add_foreign_key "answers", "elements", :name => "answers_ibfk_1", :column => "question_id"
-
-  add_foreign_key "organization_memberships", "organizations", :name => "organization_memberships_ibfk_2", :dependent => :delete
-
   add_foreign_key "organizational_roles", "organizations", :name => "organizational_roles_ibfk_1", :dependent => :delete
 
-  add_foreign_key "sms_keywords", "organizations", :name => "sms_keywords_ibfk_4", :dependent => :delete
+  add_foreign_key "sms_keywords", "organizations", :name => "sms_keywords_ibfk_2"
   add_foreign_key "sms_keywords", "surveys", :name => "sms_keywords_ibfk_3", :dependent => :nullify
 
   add_foreign_key "surveys", "organizations", :name => "surveys_ibfk_1", :dependent => :delete
