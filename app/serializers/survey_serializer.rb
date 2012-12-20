852efbac1f6a3475a62a30a95fbfbd497b0a667a
croemmich <chris.roemmich@cojourners.com>
2012-12-20 16:40:54 -0600

    expose survey elements expose all_questions and archived_questions clean up person serializer

--- app/serializers/survey_serializer.rb
+++ app/serializers/survey_serializer.rb
@@ -1,10 +1,12 @@
 class SurveySerializer < ActiveModel::Serializer
-  INCLUDES = [:questions, :keyword]
+  INCLUDES = [:questions, :all_questions, :archived_questions, :keyword]
 
   attributes :id, :title, :organization_id, :post_survey_message, :terminology, :login_paragraph,
              :is_frozen, :created_at, :updated_at
 
   has_many :questions, :serializer => QuestionSerializer
+  has_many :all_questions, :serializer => QuestionSerializer
+  has_many :archived_questions, :serializer => QuestionSerializer
   has_one :keyword
 
   def include_associations!
@@ -16,14 +18,8 @@ class SurveySerializer < ActiveModel::Serializer
       end
     end if includes
   end
-
+  
   INCLUDES.each do |relationship|
-    define_method(relationship) do
-      add_since(object.send(relationship))
-    end
-  end
-
-  [:questions, :keyword].each do |relationship|
     define_method(relationship) do
       add_since(object.send(relationship))
     end
