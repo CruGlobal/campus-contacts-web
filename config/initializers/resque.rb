7de86d9ba91be94b2a91f639ac66b13cf706d2d7
Josh Starcher <josh.starcher@gmail.com>
2012-12-20 07:59:03 -0500

    Use dedicated redis server for resque instead of mh1

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,8 +3,10 @@ require 'resque/server'
 rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 
-resque_config = YAML.load_file(rails_root + '/config/resque.yml')
-Resque.redis = resque_config[rails_env]
+#resque_config = YAML.load_file(rails_root + '/config/resque.yml')
+Resque.redis = $redis #resque_config[rails_env]
+
+Resque.redis.namespace = "MH:#{rails_env}:resque"
 
 if APP_CONFIG['resque_username']
   Resque::Server.use Rack::Auth::Basic do |username, password|
