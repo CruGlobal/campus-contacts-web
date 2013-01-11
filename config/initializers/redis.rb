064dee14639c858bb236937c4a099c721526160a
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2013-01-11 14:52:44 +0800

    Clear redisdb in test

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,4 +3,6 @@ when Rails.env.production?
   $redis = Redis.new(:host => '10.178.202.203', :port => 6379)
 else
   $redis = Redis.new(:host => '127.0.0.1', :port => 6379)
+  $redis.flushdb if Rails.env = "test"
 end
+
