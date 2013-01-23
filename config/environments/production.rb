29a4b3285abd38c18fb2b45aa7201d9889a2b221
Josh Starcher <josh.starcher@gmail.com>
2013-01-23 11:34:10 -0500

    go back to using session stores on mh1/mh2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,7 @@ Mh::Application.configure do
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
-  #cache_servers = ['10.180.4.101', '10.180.4.111']
-  cache_servers = ['10.181.129.61']
+  cache_servers = ['10.180.4.101', '10.180.4.111']
 
   config.cache_store = :dalli_store, cache_servers,  { :namespace => 'MissionHubCache', :expire_after => 1.day, :compress => true }
 
