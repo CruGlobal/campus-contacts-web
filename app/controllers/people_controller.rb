e7104fc16a29c88e1278ffd93435c4f14f77de3d
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2013-03-09 00:02:57 +0800

    Fix updating of friends

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -45,7 +45,7 @@ class PeopleController < ApplicationController
     @person = Person.find(params[:id])
     @assigned_tos = @person.assigned_tos.where('contact_assignments.organization_id' => current_organization.id).collect { |a| a.assigned_to.try(:name) }.compact.to_sentence
     authorize!(:read, @person)
-    if current_person.friends.count == 0
+    if @person.friends.count == 0
       fb_auth = @person.user.authentications.first
       @person.update_friends(fb_auth) if fb_auth.present?
     end
