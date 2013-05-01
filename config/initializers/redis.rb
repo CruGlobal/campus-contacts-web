4fe99bb47c06ab5b94698243cc35efc848bc483f
Josh Starcher <josh.starcher@gmail.com>
2013-05-01 14:12:54 -0400

    Use redis hostname instead of an ip address

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 case
 when Rails.env.production?
-  $redis = Redis.new(:host => '10.178.202.203', :port => 6379)
+  $redis = Redis.new(:host => 'redis', :port => 6379)
 else
   $redis = Redis.new(:host => '127.0.0.1', :port => 6379)
   $redis.flushdb if Rails.env = "test"
