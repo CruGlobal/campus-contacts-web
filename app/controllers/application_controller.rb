6e805e02a87bd58d2fabc8586c9b71c1fdcc43ec
Josh Starcher <josh.starcher@gmail.com>
2013-05-02 15:20:29 -0400

    Add an aws subdomain for testing aws

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -185,7 +185,7 @@ class ApplicationController < ActionController::Base
 
   def check_valid_subdomain
     return if request.subdomains.first.blank?
-    unless (available_locales + %w[www local stage]).include?(request.subdomains.first)
+    unless (available_locales + %w[www local stage aws]).include?(request.subdomains.first)
       redirect_to 'http://' + request.host_with_port.sub(request.subdomains.first, 'www') and return false
     end
   end
@@ -360,15 +360,15 @@ class ApplicationController < ActionController::Base
     end
   end
   helper_method :current_user_super_admin?
-  
-  def format_date_for_search(input_date)   	
+
+  def format_date_for_search(input_date)
 		if input_date =~ /^([1-9]|0[1-9]|1[012])\/([1-9]|0[1-9]|[12][1-9]|3[01])\/(19|2\d)\d\d$/
 			begin
 				get_date = input_date.split('/')
 				new_date = Date.parse("#{get_date[2]}-#{get_date[0]}-#{get_date[1]}").strftime("%Y-%m-%d")
 			rescue
 			end
-		end     	
+		end
 		new_date.present? ? new_date : input_date
   end
 end
