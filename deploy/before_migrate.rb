a3c4024bf40cb0b6c95dcbc4b48e597dd76026e0
Josh Starcher <josh.starcher@gmail.com>
2013-05-02 12:57:44 -0400

    comment out before_migrate

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,23 +1,23 @@
-include_recipe "missionhub::configure"
+#include_recipe "missionhub::configure"
 
-Chef::Log.info("Running deploy/before_migrate.rb...")
+#Chef::Log.info("Running deploy/before_migrate.rb...")
 
-Chef::Log.info("Symlinking #{release_path}/public/assets to #{new_resource.deploy_to}/shared/assets")
+#Chef::Log.info("Symlinking #{release_path}/public/assets to #{new_resource.deploy_to}/shared/assets")
 
-directory "#{new_resource.deploy_to}/shared/assets" do
-  action :create
-end
+#directory "#{new_resource.deploy_to}/shared/assets" do
+  #action :create
+#end
 
-link "#{release_path}/public/assets" do
-  to "#{new_resource.deploy_to}/shared/assets"
-end
+#link "#{release_path}/public/assets" do
+  #to "#{new_resource.deploy_to}/shared/assets"
+#end
 
-rails_env = new_resource.environment["RAILS_ENV"]
-Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
+#rails_env = new_resource.environment["RAILS_ENV"]
+#Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
 
-execute "rake assets:precompile" do
-  cwd release_path
-  command "bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => rails_env
-end
+#execute "rake assets:precompile" do
+  #cwd release_path
+  #command "bundle exec rake assets:precompile"
+  #environment "RAILS_ENV" => rails_env
+#end
 
