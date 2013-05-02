aee8e29ff9d5bee4dec5cb06284828672d4cc278
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2013-05-02 23:51:14 +0800

    Add interaction model relationships

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -1,3 +1,10 @@
 class Interaction < ActiveRecord::Base
   attr_accessible :comment, :created_by_id, :deleted_at, :interaction_type_id, :organization_id, :privacy_setting, :receiver_id, :timestamp
+
+  has_many :interaction_initiators
+  has_many :initiators, through: :interaction_initiators, source: :person
+  belongs_to :organization
+  belongs_to :interaction_type
+  belongs_to :receiver, class_name: 'Person', foreign_key: 'receiver_id'
+  belongs_to :creator, class_name: 'Person', foreign_key: 'created_by_id'
 end
