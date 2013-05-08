6f5c43f395d67e836b9cf90a601ab8b7da88cb4f
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2013-05-09 00:34:03 +0800

    Display intractions

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -8,4 +8,35 @@ class Interaction < ActiveRecord::Base
   belongs_to :receiver, class_name: 'Person', foreign_key: 'receiver_id'
   belongs_to :creator, class_name: 'Person', foreign_key: 'created_by_id'
   
+  scope :recent, order('created_at DESC')
+  
+  def initiator
+    self.initiators.first
+  end
+  
+  def icon
+    file_name = interaction_type.i18n
+    file_name = 'comment' if file_name == 'faculty_on_mission'
+    return "#{file_name}.png"
+  end
+  
+  def title
+    case interaction_type.i18n
+    when 'comment'
+      return "<strong>#{self.initiator}</strong> shared a comment.".html_safe
+    when 'spiritual_conversation'
+      return "<strong>#{self.initiator}</strong> initiated spiritual conversation with <strong>#{receiver}</strong>.".html_safe
+    when 'gospel_presentation'
+      return "<strong>#{self.initiator}</strong> shared the gospel with <strong>#{receiver}</strong>.".html_safe
+    when 'prayed_to_receive_christ'
+      return "<strong>#{self.initiator}</strong> led <strong>#{receiver}</strong> to pray to receive Christ.".html_safe
+    when 'holy_spirit_presentation'
+      return "<strong>#{self.initiator}</strong> shared the holy spirit presentation with <strong>#{receiver}</strong>.".html_safe
+    when 'graduating_on_mission'
+      return "<strong>#{self.initiator}</strong> helped <strong>#{receiver}</strong> develop a plan for graduating on a mission.".html_safe
+    when 'faculty_on_mission'
+      return "<strong>#{self.initiator}</strong> helped <strong>#{receiver}</strong> develop a plan to be a faculty member on a mission.".html_safe
+    end
+  end
+  
 end
