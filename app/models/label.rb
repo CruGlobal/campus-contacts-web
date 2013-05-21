e0ff08cedb9e42450dd1ad1f97faa268888062a0
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2013-05-22 00:32:26 +0800

    Add label structure relationship

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,3 +1,13 @@
 class Label < ActiveRecord::Base
   attr_accessible :i18n, :name, :organization_id
+
+  has_many :people, through: :organizational_labels
+  has_many :organizational_labels, inverse_of: :label, dependent: :destroy
+  belongs_to :organization, inverse_of: :labels
+
+  validates :i18n, uniqueness: true, allow_nil: true
+  validates :name, presence: true, :if => Proc.new { |role| organization_id != 0 }
+  validates :organization_id, presence: true
+
+  scope :default, where(organization_id: 0) if Label.table_exists? # added for travis testing
 end
--- app/models/organizational_label.rb
+++ app/models/organizational_label.rb
@@ -1,3 +1,16 @@
 class OrganizationalLabel < ActiveRecord::Base
   attr_accessible :added_by_id, :label_id, :organization_id, :person_id, :removed_date, :start_date
+  before_create :set_start_date
+  
+  belongs_to :person, :touch => true
+  belongs_to :created_by, class_name: 'Person', foreign_key: 'added_by_id'
+  belongs_to :label
+  belongs_to :organization
+  
+  private
+
+    def set_start_date
+      self.start_date = Date.today
+      true
+    end
 end
