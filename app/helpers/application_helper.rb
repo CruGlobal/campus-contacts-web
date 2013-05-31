4e0f63dbfe6fb8a059d6cdcaa9b26eb8d0d3b9fc
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2013-06-01 02:20:47 +0800

    Add more tooltips

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -98,7 +98,7 @@ module ApplicationHelper
   end
 
   def link_to_remove_fields(f, hidden)
-    f.hidden_field(:_destroy) + link_to(' ', '#', class: 'remove_field', style: hidden ? 'display:none' : '')
+    f.hidden_field(:_destroy) + link_to(' ', '#', class: 'remove_field tip', title: 'Remove', style: hidden ? 'display:none' : '')
   end
 
   def link_to_add_fields(name, f, association)
