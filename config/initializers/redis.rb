94cfabe993b3a93e21b4e047451cf2f37ee81d1c
Josh Starcher <josh.starcher@gmail.com>
2013-07-03 10:26:51 -0600

    Get rid of the flushdb command

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,5 +2,3 @@ redis_config = YAML.load_file(Rails.root.join('config','redis.yml'))
 server, port = redis_config[Rails.env].split(':')
 
 $redis = Redis.new(:host => server, :port => port)
-
-$redis.flushdb if Rails.env = "test"
