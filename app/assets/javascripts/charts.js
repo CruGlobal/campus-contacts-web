04b915e407fd618206e0d0ca6762e1f05fed1a86
Justin Sabelko <justin.sabelko@cojourners.com>
2013-07-12 01:38:11 -0500

    Spinnery goodness

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -11,3 +11,9 @@ $(document).ready(function() {
     }
   });
 });
+$(document).ready(function() {
+  $('div#snapshot_movements_submit input.large_gray').bind('click', function () {
+    $('div#snap_container1, div#snap_container2, div#snap_container3').hide();
+    $('div#snap_spinner').show();
+  });
+});
