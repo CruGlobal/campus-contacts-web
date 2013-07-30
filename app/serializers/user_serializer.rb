75129d38fadded9590111b59e286177f66e4de4f
Chris Roemmich <chris@cr-wd.com>
2013-07-30 13:42:28 -0500

    APIv3: primary organization id should be derived if not set

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,5 +2,9 @@ class UserSerializer < ActiveModel::Serializer
 
   attributes :id, :primary_organization_id, :created_at, :updated_at
 
+  def primary_organization_id
+    object.person.primary_organization.id
+  end
+
 end
 
