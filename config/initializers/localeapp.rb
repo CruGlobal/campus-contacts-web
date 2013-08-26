9d08b240344ab63cc674e67aade8c20783919fb9
Josh Starcher <josh.starcher@gmail.com>
2013-08-26 09:04:15 -0400

    Update gems

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,11 +1,11 @@
-if defined?(Rails) && Rails.env.development?
-  require 'localeapp'
-  require 'localeapp/rails'
-  begin
-    Localeapp.configure do |config|
-      config.api_key = 'cVxc9XRMMwMHpYBAaKUvFjzeJscAHUo5nMxNs9jilxP9zBTzEu'
-      config.sending_environments = []
-    end
-  end
-end
+#if defined?(Rails) && Rails.env.development?
+  #require 'localeapp'
+  #require 'localeapp/rails'
+  #begin
+    #Localeapp.configure do |config|
+      #config.api_key = 'cVxc9XRMMwMHpYBAaKUvFjzeJscAHUo5nMxNs9jilxP9zBTzEu'
+      #config.sending_environments = []
+    #end
+  #end
+#end
 
