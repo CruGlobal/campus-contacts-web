fa4abdeb532603dc1ccd04d474c589793c563089
Justin Sabelko <justin.sabelko@cojourners.com>
2013-09-12 13:42:14 -0500

    Validation for goal dates

--- app/models/organizational_goal.rb
+++ app/models/organizational_goal.rb
@@ -1,7 +1,14 @@
 class OrganizationalGoal < ActiveRecord::Base
   belongs_to :organization
 
+  validate :date_check
   validates_presence_of :start_value, :start_date, :end_value, :end_date, :organization_id, :criteria
   validates_numericality_of :start_value, :end_value
   validates_uniqueness_of :criteria, scope: :organization_id
+
+  def date_check
+    if start_date >= end_date
+      errors.add(:start_date, "must be before End date")
+    end
+  end
 end
