73abd3bacee3403a10084e7066d3bfaac5f22c6f
Josh Starcher <josh.starcher@gmail.com>
2013-10-23 12:02:25 -0400

    Sidekiq config should use MH in the namespace, not MPDX

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,5 +5,5 @@ redis_config = YAML.load_file(rails_root + '/config/redis.yml')
 
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://' + redis_config[rails_env],
-                   namespace: "MPDX:#{rails_env}:resque"}
+                   namespace: "MH:#{rails_env}:resque"}
 end
