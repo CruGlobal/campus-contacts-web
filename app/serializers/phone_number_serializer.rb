43d8d35e99d8e13648f70115d9329f1d1ebbe580
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2013-12-10 13:33:17 +0800

    Display tag and indicate in API the opted out mobile numbers

--- app/serializers/phone_number_serializer.rb
+++ app/serializers/phone_number_serializer.rb
@@ -1,6 +1,10 @@
 class PhoneNumberSerializer < ActiveModel::Serializer
 
-  attributes :id, :person_id, :number, :location, :primary, :txt_to_email, :email_updated_at, :created_at, :updated_at
+  attributes :id, :person_id, :number, :location, :primary, :txt_to_email, :email_updated_at, :created_at, :updated_at, :opted_out
+
+  def opted_out
+    object.inactive_from_orgs
+  end
 
 end
 
