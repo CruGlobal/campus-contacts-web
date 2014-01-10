9f6af78d4c6bb57ae698f2dfdc117b41e50b2e24
Marvin Punongbayan <marvindpunongbayan@gmail.com>
2014-01-10 22:54:42 +0800

    Return to the old process

--- app/models/person.rb
+++ app/models/person.rb
@@ -1308,7 +1308,9 @@ class Person < ActiveRecord::Base
 
   def self.find_existing_person_by_email(email)
     return unless email.present?
-    (User.find_by_username(email) || User.find_by_email(email) || EmailAddress.find_by_email(email)).try(:person)
+    (EmailAddress.find_by_email(email) ||
+      User.find_by_username(email) ||
+      User.find_by_email(email)).try(:person)
   end
 
   def self.find_existing_person_by_email_address(email_address)
