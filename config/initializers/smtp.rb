3344d2967e21ef6183d36e4d12583f8b1a0048b9
Josh Starcher <josh.starcher@gmail.com>
2014-01-28 16:18:37 -0500

    Add domain to smtp config

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,6 +2,7 @@ require Rails.root.join('config','initializers','load_config').to_s
 ActionMailer::Base.smtp_settings = {
   :user_name => APP_CONFIG['smtp_user_name'],
   :password => APP_CONFIG['smtp_password'],
+  :domain => "missionhub.com",
   :address => APP_CONFIG['smtp_address'],
   :authentication => (APP_CONFIG['smtp_authentication'] || :none),
   :enable_starttls_auto => (APP_CONFIG['smtp_enable_starttls_auto']),
