b31dbccfae114bb3a6a6fc7693ad7cfb7884fdf5
Marvin Punongbayan <marvindpunongbayan@gmail.com>
2014-08-11 17:21:07 +0800

    Syncronize output for generate csv of contacts list

--- app/models/contacts_csv_generator.rb
+++ app/models/contacts_csv_generator.rb
@@ -7,17 +7,22 @@ class ContactsCsvGenerator
       rows << [t('contacts.index.first_name'), t('contacts.index.last_name'), t('general.status'), t('general.assigned_to'), t('general.gender'), t('contacts.index.phone_number'), t('people.index.email')] + questions.collect {|q| q.label} + [t('contacts.index.last_survey')]
       people.each do |person|
         answers = [person.first_name, person.last_name, permissions[person.id].present? ? permissions[person.id].followup_status.to_s.titleize : nil, person.assigned_tos_by_org(organization).collect{|a| Person.find(a.assigned_to_id).name}.join(','), person.gender.to_s.titleize, person.pretty_phone_number, person.email]
-        dates = []
         questions.each do |q|
           answer = all_answers[person.id][q.id]
-          if answer
-            answers << answer.first
-            dates << answer.last
+          if q.slug == 'email'
+            answers << person.email
+          elsif q.object_name == 'person'
+            answers << person.send(:"#{q.attribute_name}")
           else
-            answers << ''
+            if answer
+              answers << answer.first
+            else
+              answers << ''
+            end
           end
         end
-        answers << I18n.l(dates.sort.reverse.last, format: :date) if dates.present?
+        last_answer_sheet = person.completed_answer_sheets(organization)
+        answers << I18n.l(last_answer_sheet.first.updated_at, format: :date) if last_answer_sheet.present?
         rows << answers
       end
     end
