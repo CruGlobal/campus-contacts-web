87c0ce3e3b0f2b4ff9e556162632d7945e0e9180
Marvin Punongbayan <marvindpunongbayan@gmail.com>
2014-11-19 04:20:48 +0800

    Fix test errors

--- test/functional/crs_import_mailer_test.rb
+++ test/functional/crs_import_mailer_test.rb
@@ -4,14 +4,14 @@ class CrsImportMailerTest < ActionMailer::TestCase
   test "CRS Import Failed" do
     @org = Factory(:organization)
     mail = CrsImportMailer.failed(@org, 'crs@example.com')
-    assert_equal "Crs Import Failed", mail.subject
+    assert_equal "CRS Import Failed", mail.subject
     assert_equal ["crs@example.com"], mail.to
   end
 
   test "CRS Import Completed" do
     @org = Factory(:organization)
     mail = CrsImportMailer.completed(@org, 'crs@example.com')
-    assert_equal "Crs Import Completed", mail.subject
+    assert_equal "CRS Import Completed", mail.subject
     assert_equal ["crs@example.com"], mail.to
   end
 end
