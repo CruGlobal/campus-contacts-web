d680faacf260747d268a95391ebe826177d3a0a2
Marvin Punongbayan <marvindpunongbayan@gmail.com>
2015-01-20 03:58:08 +0800

    Force to setup host the facebook_mhub in mhub.cc omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,7 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, FB_APP_ID, FB_SECRET, scope: 'user_birthday,email,offline_access,user_interests,user_location,user_education_history', setup: OmniauthHostSetup
-  provider :facebook_mhub, FB_APP_ID_MHUB, FB_SECRET_MHUB, scope: 'user_birthday,email,user_interests,user_location,user_education_history'
+  provider :facebook_mhub, FB_APP_ID_MHUB, FB_SECRET_MHUB, scope: 'user_birthday,email,user_interests,user_location,user_education_history', setup: OmniauthHostSetup
   provider :cas, name: 'relay', url: 'https://signin.relaysso.org/cas', setup: OmniauthHostSetup
   provider :cas, name: 'key', url: 'https://thekey.me/cas', setup: OmniauthHostSetup
 end
