88bfe226a9bd767841ded6e39dcf5ef53cb6f824
Marvin Punongbayan <marvindpunongbayan@gmail.com>
2015-06-02 04:15:27 +0800

    Remove permission for blank name contacts in FamilyLife Organization5606

---
+++ db/migrate/20150601193609_fix_data_remove_blank_name_contacts_in_organization5606.rb
@@ -0,0 +1,32 @@
+class FixDataRemoveBlankNameContactsInOrganization5606 < ActiveRecord::Migration
+  def up
+    # Organization that has blank name contacts
+    org_id = 5606
+
+    org = Organization.find_by_id(org_id)
+    if org.present?
+      blank_name_contacts = org.all_people.where("(people.last_name IS NULL AND people.first_name IS NULL) OR (people.last_name = '' AND people.first_name = '')")
+      if blank_name_contacts.present?
+        blank_name_contacts.each do |contact|
+          permission = contact.all_organizational_permissions.find_by_organization_id(org.id)
+          permission.destroy if permission.present?
+        end
+      else
+        puts "There's no contacts that has blank name."
+      end
+
+      # Removing all organizational permissions that person data does not exist in the database
+      org_permissions = org.organizational_permissions
+      if org_permissions.present?
+        org_permissions.each do |org_permission|
+          org_permission.destroy unless org_permission.person.present?
+        end
+      end
+    else
+      puts "Can't find the organization."
+    end
+  end
+
+  def down
+  end
+end
