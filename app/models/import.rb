98c0a91dd9db6c4bd068379c2b165d22984c7208
Josh Starcher <josh.starcher@gmail.com>
2015-07-08 21:04:38 -0600

    Use env vars for paperclip s3 config.

--- app/models/import.rb
+++ app/models/import.rb
@@ -17,7 +17,8 @@ class Import < ActiveRecord::Base
   belongs_to :user
   belongs_to :organization
 
-  has_attached_file :upload, s3_credentials: 'config/config.yml', s3_permissions: :private, storage: :s3, path: 'mh/imports/:attachment/:id/:filename', s3_storage_class: :reduced_redundancy
+  has_attached_file :upload, s3_credentials: {:bucket => ENV['BUCKET'], :access_key_id => ENV['ACCESS_KEY_ID'], :secret_access_key => ENV['SECRET_ACCESS_KEY']},
+    s3_permissions: :private, storage: :s3, path: 'mh/imports/:attachment/:id/:filename', s3_storage_class: :reduced_redundancy
 
   validates_attachment_file_name :upload, :matches => [/csv\Z/]
 
