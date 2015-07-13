ebc1e054e9ccc485356bdedd11d426723b36a925
Josh Starcher <josh.starcher@gmail.com>
2015-07-13 09:57:44 -0600

    Make sure that www redirects to ssl.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def ssl_configured?
-    !Rails.env.development? && !Rails.env.test? && request.subdomains.first.blank?
+    !Rails.env.development? && !Rails.env.test? && !available_locales.include?(request.subdomains.first)
   end
 
   def clear_advanced_search
