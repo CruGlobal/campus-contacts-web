66e9450e1bfa327167f2cc439c0f0aa9063866e8
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2015-08-20 02:16:00 +0800

    MH-1251 Fix phone number duplicate entry error

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -272,7 +272,15 @@ class PeopleController < ApplicationController
         end
       end
 
-      if @person.update_attributes(params[:person])
+      # This might fix the duplicate entry error
+      save_completed = false
+      begin
+        save_completed = @person.update_attributes(params[:person])
+      rescue ActiveRecord::RecordNotUnique
+        retry
+      end
+
+      if save_completed
         @person.update_attribute(:student_status, nil) if @person.faculty?
         @person.update_date_attributes_updated
         format.html { redirect_to(@person, notice: 'Person was successfully updated.') }
