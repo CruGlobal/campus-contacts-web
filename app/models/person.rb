d1ee143fd5a492d2338d3ab14450c76041cb692e
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2015-08-21 01:18:45 +0800

    Handle orgs without any labels

--- app/models/person.rb
+++ app/models/person.rb
@@ -1067,7 +1067,7 @@ class Person < ActiveRecord::Base
   end
 
   def labels_by_org_id(org_id)
-    if @labels_by_org_id.nil? && row['label_ids'].present?
+    if @labels_by_org_id.nil?
       @labels_by_org_id = Hash[OrganizationalLabel.connection.select_all("select organization_id, group_concat(label_id) as label_ids from organizational_labels where person_id = #{id} and removed_date is NULL group by organization_id").collect { |row| [row['organization_id'], row['label_ids'].present? ? row['label_ids'].split(',').map(&:to_i) : [] ]}]
     end
     @labels_by_org_id[org_id]
