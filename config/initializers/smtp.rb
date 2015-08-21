d732d8c9f19019beef44187b4726cc7383dc2d2b
Marvin Punongbayan <marvindpunongbayan@gmail.com>
2015-08-21 16:58:56 +0800

    Switch Sendgrid into aws-ses

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,8 @@
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV.fetch('SMTP_USER_NAME'),
   :password => ENV.fetch('SMTP_PASSWORD'),
-  :domain => "missionhub.com",
   :address => ENV.fetch('SMTP_ADDRESS'),
-  :authentication => (ENV['SMTP_AUTHENTICATION'] || :none),
-  :enable_starttls_auto => (ENV.fetch('SMTP_ENABLE_STARTTLS_AUTO')),
-  :port => ENV['SMTP_PORT'] || 25
+  :authentication => (ENV.fetch('SMTP_AUTHENTICATION') || :none),
+  :enable_starttls_auto => ENV.fetch('SMTP_ENABLE_STARTTLS_AUTO'),
+  :port => ENV.fetch('SMTP_PORT') || 25
 }
