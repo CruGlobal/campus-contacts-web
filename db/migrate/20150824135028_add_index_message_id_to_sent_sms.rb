e0aa5052c7cbce0572f3ffc123b9dc249d0ed96d
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2015-08-24 21:55:43 +0800

    Add index in sent_sms and allow emoji

---
+++ db/migrate/20150824135028_add_index_message_id_to_sent_sms.rb
@@ -0,0 +1,6 @@
+class AddIndexMessageIdToSentSms < ActiveRecord::Migration
+  def change
+    add_index :sent_sms, [:message_id], :name => "index_sent_sms_on_message_id", :unique => false
+    execute "ALTER TABLE sent_sms MODIFY message TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
+  end
+end
