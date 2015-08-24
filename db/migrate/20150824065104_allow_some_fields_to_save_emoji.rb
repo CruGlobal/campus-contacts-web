96587e5d11b1faf326029524958bd0088790b438
Josh Starcher <josh.starcher@gmail.com>
2015-08-24 16:51:46 -0400

    These indexes don't exist on production

--- db/migrate/20150824065104_allow_some_fields_to_save_emoji.rb
+++ db/migrate/20150824065104_allow_some_fields_to_save_emoji.rb
@@ -1,11 +1,11 @@
 class AllowSomeFieldsToSaveEmoji < ActiveRecord::Migration
   def change
     execute "ALTER TABLE bulk_messages MODIFY results LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
-    execute "DROP INDEX index_received_sms_on_phone_number_and_message_and_received_at ON received_sms;"
+    #execute "DROP INDEX index_received_sms_on_phone_number_and_message_and_received_at ON received_sms;"
     execute "ALTER TABLE received_sms MODIFY message TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
     execute "ALTER TABLE messages MODIFY message TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
     execute "ALTER TABLE answers MODIFY value TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
-    execute "DROP INDEX index_ma_answers_on_short_value ON answers;"
+    #execute "DROP INDEX index_ma_answers_on_short_value ON answers;"
     execute "ALTER TABLE answers MODIFY short_value TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
   end
 end
