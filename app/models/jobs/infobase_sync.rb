069ab4fc856fdf4bc5a62603f9f8ba3e8cd7069f
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2015-08-26 15:00:48 +0800

    Paginate teams in infobase sync process

--- app/models/jobs/infobase_sync.rb
+++ app/models/jobs/infobase_sync.rb
@@ -59,67 +59,76 @@ class Jobs::InfobaseSync
         # Import teams
         Rails.logger.debug "-- Importing teams..."
         includes = 'people,phone_numbers,email_addresses'
-        team_json = Infobase::Team.get(include: includes)
-        loop do
-          team_json['teams'].each do |team|
-            Rails.logger.debug "---- Import Team - #{team['name']}"
-            attribs = {name: team['name'], terminology: 'Missional Team', importable_id: team['id'], importable_type: 'Ccc::MinistryLocallevel', show_sub_orgs: true, status: 'active'}
+        page = 1
+        begin
+          api_response = Infobase::Team.get(include: includes, page: page, per_page: 10)
 
-            mh_team = Organization.where(importable_id: team['id'], importable_type: 'Ccc::MinistryLocallevel').first
-            if mh_team
-              mh_team.update_attributes(attribs)
-            else
-              if team['lane'] == 'SV'
-                mh_team = chs.children.create!(attribs)
+          meta = api_response['meta']
+          page = meta['page']
+          last_record = meta['to']
+          total_record = meta['total']
+
+          team_json = api_response['teams']
+          if team_json.present?
+            team_json.each do |team|
+              Rails.logger.debug "---- Import Team - #{team['name']}"
+              attribs = {name: team['name'], terminology: 'Missional Team', importable_id: team['id'], importable_type: 'Ccc::MinistryLocallevel', show_sub_orgs: true, status: 'active'}
+
+              mh_team = Organization.where(importable_id: team['id'], importable_type: 'Ccc::MinistryLocallevel').first
+              if mh_team
+                mh_team.update_attributes(attribs)
               else
-                region = regions[team['region']]
-                next unless region
-                mh_team = region.children.create!(attribs)
-              end
-            end
-
-            teams[team['id']] = mh_team
-            Rails.logger.debug "------ Importing people..."
-            team['people'].each do |ccc_person|
-              ImportMethods.person_from_api(ccc_person, mh_team, 'admin')
-            end
-
-
-            Rails.logger.debug "------ Importing activity rows..."
-            i = 0
-            activity_json = Infobase::Activity.get('filters[team_id]' => team['id'], 'include' => 'target_area', per_page: 10000)['activities']
-            activity_json.each do |activity|
-              if activity['target_area'].present?
-                i += 1
-                Rails.logger.debug i if i % 1000 == 0
-                mh_activity = Organization.where(importable_id: activity['id'], importable_type: 'Ccc::MinistryActivity').first
-                attribs = {name: "#{strategies[activity['strategy']]} at #{activity['target_area']['name']}", terminology: 'Movement', importable_id: activity['id'], importable_type: 'Ccc::MinistryActivity', status: 'active'}
-                if mh_activity
-                  mh_activity.update_attributes(attribs)
-
-                  if mh_activity.parent_organization != mh_team
-                    mh_activity.parent_organization = mh_team
-                  end
-
-                  # Ensure that the parents are synchronized
-                  mh_activity.fix_ancestry
+                if team['lane'] == 'SV'
+                  mh_team = chs.children.create!(attribs)
                 else
-                  next unless mh_team
-                  mh_activity = mh_team.children.create!(attribs)
-                  begin
-                    mh_activity.save!
-                  rescue => e
-                    Rails.logger.info e.inspect
+                  region = regions[team['region']]
+                  next unless region
+                  mh_team = region.children.create!(attribs)
+                end
+              end
+
+              teams[team['id']] = mh_team
+              Rails.logger.debug "------ Importing people..."
+              team['people'].each do |ccc_person|
+                ImportMethods.person_from_api(ccc_person, mh_team, 'admin')
+              end
+
+
+              Rails.logger.debug "------ Importing activity rows..."
+              i = 0
+              activity_json = Infobase::Activity.get('filters[team_id]' => team['id'], 'include' => 'target_area', per_page: 10000)['activities']
+              activity_json.each do |activity|
+                if activity['target_area'].present?
+                  i += 1
+                  Rails.logger.debug i if i % 1000 == 0
+                  mh_activity = Organization.where(importable_id: activity['id'], importable_type: 'Ccc::MinistryActivity').first
+                  attribs = {name: "#{strategies[activity['strategy']]} at #{activity['target_area']['name']}", terminology: 'Movement', importable_id: activity['id'], importable_type: 'Ccc::MinistryActivity', status: 'active'}
+                  if mh_activity
+                    mh_activity.update_attributes(attribs)
+
+                    if mh_activity.parent_organization != mh_team
+                      mh_activity.parent_organization = mh_team
+                    end
+
+                    # Ensure that the parents are synchronized
+                    mh_activity.fix_ancestry
+                  else
+                    next unless mh_team
+                    mh_activity = mh_team.children.create!(attribs)
+                    begin
+                      mh_activity.save!
+                    rescue => e
+                      Rails.logger.info e.inspect
+                    end
                   end
                 end
               end
             end
           end
+          page += 1
+        end while last_record < total_record
 
-          break unless team_json['meta']['to'] < team_json['meta']['total']
-          # get the next page of teams
-          team_json = Infobase::Team.get(page: team_json['meta']['page'] + 1, include: includes)
-        end
+
       end
     end
   end
