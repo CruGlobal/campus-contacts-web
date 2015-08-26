d852bae5608676b571c7e3ed63b4e01ad73f7c8e
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2015-08-26 14:06:14 +0800

    Fix database collation and adjust existing migration

--- db/migrate/20150824065104_allow_some_fields_to_save_emoji.rb
+++ db/migrate/20150824065104_allow_some_fields_to_save_emoji.rb
@@ -1,11 +1,15 @@
 class AllowSomeFieldsToSaveEmoji < ActiveRecord::Migration
   def change
     execute "ALTER TABLE bulk_messages MODIFY results LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
-    #execute "DROP INDEX index_received_sms_on_phone_number_and_message_and_received_at ON received_sms;"
+    begin
+      execute "DROP INDEX index_received_sms_on_phone_number_and_message_and_received_at ON received_sms;"
+    rescue; end
     execute "ALTER TABLE received_sms MODIFY message TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
     execute "ALTER TABLE messages MODIFY message TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
     execute "ALTER TABLE answers MODIFY value TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
-    #execute "DROP INDEX index_ma_answers_on_short_value ON answers;"
+    begin
+      execute "DROP INDEX index_ma_answers_on_short_value ON answers;"
+    rescue; end
     execute "ALTER TABLE answers MODIFY short_value TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
   end
 end
