bf2f19c0eea85aec6a7462c5b9454b977100378c
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2015-09-02 15:49:19 +0800

    Uncomment encoding restoration migration

--- db/migrate/20150901024149_restore_table_columns_utf8_encoding.rb
+++ db/migrate/20150901024149_restore_table_columns_utf8_encoding.rb
@@ -1,11 +1,11 @@
 class RestoreTableColumnsUtf8Encoding < ActiveRecord::Migration
   def change
-    # execute "ALTER TABLE bulk_messages MODIFY results LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci;"
-    # execute "ALTER TABLE received_sms MODIFY message TEXT CHARACTER SET utf8 COLLATE utf8_general_ci;"
-    # execute "ALTER TABLE messages MODIFY message TEXT CHARACTER SET utf8 COLLATE utf8_general_ci;"
-    # execute "ALTER TABLE answers MODIFY value TEXT CHARACTER SET utf8 COLLATE utf8_general_ci;"
-    # execute "ALTER TABLE answers MODIFY short_value TEXT CHARACTER SET utf8 COLLATE utf8_general_ci;"
-    # execute "ALTER TABLE sent_sms MODIFY message TEXT CHARACTER SET utf8 COLLATE utf8_general_ci;"
-    # execute "ALTER TABLE interactions MODIFY comment TEXT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+    execute "ALTER TABLE bulk_messages MODIFY results LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+    execute "ALTER TABLE received_sms MODIFY message TEXT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+    execute "ALTER TABLE messages MODIFY message TEXT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+    execute "ALTER TABLE answers MODIFY value TEXT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+    execute "ALTER TABLE answers MODIFY short_value TEXT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+    execute "ALTER TABLE sent_sms MODIFY message TEXT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+    execute "ALTER TABLE interactions MODIFY comment TEXT CHARACTER SET utf8 COLLATE utf8_general_ci;"
   end
 end
