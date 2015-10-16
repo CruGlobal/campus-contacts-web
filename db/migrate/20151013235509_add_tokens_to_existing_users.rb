d4f6362e47adff2ec2fd60a6e0e37a05d3635c27
Spencer Oberstadt <soberstadt@gmail.com>
2015-10-16 15:01:57 -0400

    remove migration because the code to run it doesn't exist anymore

--- db/migrate/20151013235509_add_tokens_to_existing_users.rb
+++ db/migrate/20151013235509_add_tokens_to_existing_users.rb
@@ -1,5 +1,6 @@
 class AddTokensToExistingUsers < ActiveRecord::Migration
   def change
-    User.where(token: nil).find_each { |u| u.regenerate_token }
+    # this was used to generate tokens, but we don't want to do that anymore
+    # User.where(token: nil).find_each { |u| u.regenerate_token }
   end
 end
