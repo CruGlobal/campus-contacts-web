6f2ff198bca991b45501392fc4b569b7282c4b6e
Spencer Oberstadt <soberstadt@gmail.com>
2016-05-17 11:06:11 -0400

    show organization names on dashboard

--- app/assets/javascripts/angular/components/myContactsDashboard.component.js
+++ app/assets/javascripts/angular/components/myContactsDashboard.component.js
@@ -6,24 +6,69 @@
             templateUrl: '/templates/myContactsDashboard.html'
         });
 
-    function angularTestController($http, state) {
+    function angularTestController($http, $q, state) {
         var vm = this;
         vm.contacts = [];
+        vm.organization_contacts = {};
         vm.loading = true;
 
-        vm.retry = activate;
+        vm.org_name = org_name;
 
         activate();
 
         function activate() {
-            $http.get('http://localhost:3001/apis/v4/people?access_token=' + state.v4_access_token)
-                 .then(function (request) {
-                     var store = new JsonApiDataStore();
-                     store.sync(request.data);
-                     vm.contacts = store.findAll('person');
-                     console.log(vm.contacts);
-                     vm.loading = false;
-                 })
+            $q.all([loadContacts(), loadOrganizations()]).then(contactsLoaded);
+        }
+
+        function loadContacts() {
+            return $http
+                .get('http://localhost:3001/apis/v4/people', {
+                    params: {
+                        access_token: state.v4_access_token, limit: 100
+                    }
+                })
+                .then(function (request) {
+                    var store = new JsonApiDataStore();
+                    store.sync(request.data);
+                    vm.contacts = store.findAll('person');
+                    vm.loading = false;
+
+                })
+        }
+
+        function loadOrganizations() {
+            return $http
+                .get('http://localhost:3001/apis/v4/organizations', {
+                    params: {
+                        access_token: state.v4_access_token,
+                        include : 'organization.name,organization.id'
+                    }
+                })
+                .then(function(request) {
+                    var store = new JsonApiDataStore();
+                    store.sync(request.data);
+                    vm.organizations = store;
+                })
+        }
+
+        function contactsLoaded() {
+            vm.organization_contacts = {};
+            angular.forEach(vm.contacts, function (contact) {
+                angular.forEach(contact.organizational_permissions, function(op) {
+                    var org_id = op.organization_id;
+                    if(vm.organization_contacts[org_id] === undefined)
+                        vm.organization_contacts[org_id] = [contact];
+                    else
+                        vm.organization_contacts[org_id].push(contact);
+                })
+            })
+        }
+
+        function org_name(org_id) {
+            var org = vm.organizations.find('organization', org_id);
+            if(org)
+                return org.name;
+            return org_id;
         }
     }
 })();
