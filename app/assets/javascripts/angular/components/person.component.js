916300ddcc9704990dbbe548cc3b1ed9a5fd721f
Spencer Oberstadt <soberstadt@gmail.com>
2016-06-06 16:50:58 -0400

    Save new interaction (#599)
    * save new interactions
    
    * auto-grow notes on new interaction
    
    * DRY up interactions panel

--- app/assets/javascripts/angular/components/person.component.js
+++ app/assets/javascripts/angular/components/person.component.js
@@ -7,25 +7,34 @@
             controller: personController,
             templateUrl: '/templates/person.html',
             bindings: {
-                'person': '<'
+                'person': '<',
+                'organizationId': '<'
             }
         });
 
-    function personController($timeout, $animate) {
+    function personController($animate, $http, $log, envService, JsonApiDataStore) {
         var vm = this;
 
         vm.addInteractionBtnsVisible = false;
-        vm.openPanelName = '';
         vm.closingInteractionButtons = false;
 
         vm.toggleInteractionBtns = toggleInteractionBtns;
         vm.openAddInteractionPanel = openAddInteractionPanel;
-        vm.closeAddInteractionPanel = closeAddInteractionPanel;
+        vm.saveInteraction = saveInteraction;
+
+        vm.interactionTypes = [
+            {id: 2, icon: 'free_breakfast', title: 'Spiritual Conversation'},
+            {id: 3, icon: 'multiline_chart', title: 'Personal Evangelism'},
+            {id: 4, icon: 'spa', title: 'Personal Evangelism Decisions'},
+            {id: 5, icon: 'whatshot', title: 'Holy Spirit Presentation'},
+            {id: 9, icon: 'free_breakfast', title: 'Discipleship / Leadership'},
+            {id: 1, icon: 'event_note', title: 'Comment Only'}
+        ];
 
         activate();
 
         function activate() {
-
+            closeAddInteractionPanel()
         }
 
         function toggleInteractionBtns(){
@@ -41,11 +50,42 @@
         }
 
         function openAddInteractionPanel(type){
-            vm.openPanelName = type;
+            vm.openPanelType = type;
+        }
+
+        function saveInteraction() {
+            var newInteraction = JsonApiDataStore.store.sync({
+                data: {
+                    type: 'interaction',
+                    attributes: {
+                        comment: vm.interactionComment,
+                        interaction_type_id: vm.openPanelType.id
+                    },
+                    relationships: {
+                        organization: {
+                            data: { id: vm.organizationId, type: 'organization' }
+                        },
+                        receiver: {
+                            data: { id: vm.person.id, type: 'person' }
+                        }
+                    }
+                }
+            });
+            $log.log(newInteraction);
+            $http
+                .post(envService.read('apiUrl') + '/interactions', newInteraction.serialize())
+                .then(function() {
+                        $log.log('posted interaction successfully');
+                        closeAddInteractionPanel();
+                    },
+                    function(error){
+                        $log.error('Error saving interaction', error);
+                    });
         }
 
         function closeAddInteractionPanel(){
-            vm.openPanelName = '';
+            vm.openPanelType = '';
+            vm.interactionComment = '';
         }
     }
 })();
