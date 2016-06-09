0ab766913514655b962af6616940acb547de4612
Spencer Oberstadt <soberstadt@gmail.com>
2016-06-09 13:44:50 -0400

    clean up rack profiler config

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -2,4 +2,5 @@ if (ENV['APP_DOMAIN'] == 'stage.missionhub.com' && ENV['MINI_PROFILER']) || Rail
   require 'rack-mini-profiler'
   # initialization is skipped so trigger it
   Rack::MiniProfilerRails.initialize!(Rails.application)
+  Rack::MiniProfiler.config.start_hidden = true
 end
