6b2a6e49f1f60c88b2b187e824d8dabe77c3e675
Spencer Oberstadt <soberstadt@gmail.com>
2016-07-25 11:21:40 -0400

    only subscribe admins to MailChimp (#638)

--- app/workers/mail_chimp_sync_worker.rb
+++ app/workers/mail_chimp_sync_worker.rb
@@ -19,7 +19,8 @@ class MailChimpSyncWorker
   def active_users_not_subscribed
     User.includes(person: :primary_email_address).where('sign_in_count > 0')
       .where('current_sign_in_at > ?', CURRENT_USER_RANGE)
-      .where(subscribed_to_updates: nil)
+      .where(organizational_permissions: { permission_id: Permission::ADMIN_ID },
+             subscribed_to_updates: nil)
   end
 
   def inactive_users_subscribed
