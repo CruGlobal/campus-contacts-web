1ce3602bebd16feec7b9f955761ec4d3bef22cf1
Spencer Oberstadt <soberstadt@gmail.com>
2016-08-04 10:25:35 -0500

    fix failing eslint test

--- app/assets/javascripts/angular/missionhubApp.config.js
+++ app/assets/javascripts/angular/missionhubApp.config.js
@@ -3,7 +3,7 @@
 
     angular
         .module('missionhubApp')
-        .config(function (envServiceProvider, ngMdIconServiceProvider) {
+        .config(function (envServiceProvider, ngMdIconServiceProvider, I18n) {
             //TODO: Remove eslint comment when bug is fixed. See https://github.com/Gillespie59/eslint-plugin-angular/issues/223
             envServiceProvider.config({ // eslint-disable-line angular/module-getter
                 domains: {
