72f22061bf9e899f051dfb269b393464e9116c27
Spencer Oberstadt <soberstadt@gmail.com>
2016-08-12 12:13:36 -0400

    clobber old assets when building (#665)

--- build_assets.sh
+++ build_assets.sh
@@ -1,3 +1,3 @@
 npm install --production
-bundle exec rake assets:clean RAILS_ENV=production &&
+bundle exec rake assets:clobber RAILS_ENV=production &&
 bundle exec rake assets:precompile RAILS_ENV=production
