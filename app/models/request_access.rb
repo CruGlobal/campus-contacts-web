d0480c6cab6cb91f87eef9218918753992ed1588
Spencer Oberstadt <soberstadt@gmail.com>
2016-08-17 11:57:13 -0400

    Improve request access (#675)
    * prevent race condition on new organization callback
    
    * only allow one submit of new request form

--- app/models/request_access.rb
+++ app/models/request_access.rb
@@ -6,23 +6,28 @@ class RequestAccess
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :first_name, :last_name, :org_name, length: { minimum: 2 }
 
+  def save
+    ActiveRecord::Base.transaction do
+      person = Person.new_from_params(person_params)
+      organization = Organization.new(organization_params)
+
+      person.save!
+      organization.organizational_permissions.new(person_id: person.id, permission_id: Permission::ADMIN_ID)
+      organization.save!
+    end
+  end
+
+  private
+
   def person_params
     { first_name: first_name, last_name: last_name, email: email }
   end
 
   def organization_params
-    { name: org_name }
-  end
-
-  def save
-    ActiveRecord::Base.transaction do
-      person = Person.new_from_params(person_params)
-      organization = Organization.new(organization_params.merge(status: 'requested',
-                                                                terminology: 'Organization'))
-
-      person.save!
-      organization.save!
-      organization.add_admin(person) unless organization.admins.present?
-    end
+    {
+      name: org_name,
+      status: 'requested',
+      terminology: 'Organization'
+    }
   end
 end
