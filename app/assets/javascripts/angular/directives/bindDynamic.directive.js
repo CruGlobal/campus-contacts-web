be999112a8cae20126613a118151d94b2d86ce0e
Caleb Cox <calebcox314@gmail.com>
2016-08-26 10:20:37 -0400

    Internationalize remaining hard-coded client-side labels

---
+++ app/assets/javascripts/angular/directives/bindDynamic.directive.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    // Inspired by http://stackoverflow.com/a/21445567
+    angular
+        .module('missionhubApp')
+        .directive('bindDynamic', function ($compile) {
+            return {
+                restrict: 'A',
+                replace: true,
+                scope: { bindDynamic: '=' },
+                link: function (scope, element) {
+                    scope.$watch('bindDynamic', function (html) {
+                        element.html(html);
+                        $compile(element.contents())(scope);
+                    });
+                }
+            };
+        });
+})();
