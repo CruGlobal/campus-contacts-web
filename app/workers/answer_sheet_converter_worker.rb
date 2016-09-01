00e9059741630bbe0a935e4d782801f4811f89a5
Spencer Oberstadt <soberstadt@gmail.com>
2016-09-01 10:22:27 -0400

    Impove survey conversion (#698)
    * only make note if there are some answers
    
    * update note if it already exists
    
    * only save note if comment is updated

--- app/workers/answer_sheet_converter_worker.rb
+++ app/workers/answer_sheet_converter_worker.rb
@@ -4,16 +4,19 @@ class AnswerSheetConverterWorker
 
   def perform(answer_sheet_id)
     as = AnswerSheet.includes({ person: [:interactions] }, :answers).find(answer_sheet_id)
-    return if as.person.blank? || as.existing_note?
-    as.to_note.save!
+    return if as.person.blank? || as.answers.none? || as.existing_note.present?
+    note = as.to_note
+    note.save! if note.comment_changed?
   end
 
-  def self.batch_perform(days_back)
+  def self.batch_perform(days_back, end_date = nil)
+    days_back = days_back.days.ago if days_back.to_i < 100
+    end_date ||= DateTime.now
     batch = Sidekiq::Batch.new
     batch.description = 'Trying to run a bunch of survey to note conversions.'
-    rows = AnswerSheet.where('created_at > ?', days_back.days.ago).pluck(:id)
+    rows = AnswerSheet.where(created_at: days_back..end_date).pluck(:id)
     batch.jobs do
-      rows.each { |row| RowWorker.perform_async(row) }
+      rows.each { |row| perform_async(row) }
     end
     Rails.logger.log "Just started Batch #{batch.bid}"
   end
