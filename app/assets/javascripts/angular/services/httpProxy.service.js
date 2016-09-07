9c1ed82abe834613a745c72df2a1547bde8fbc81
Emmanuel Ijeh <emanijeh@gmail.com>
2016-09-07 15:37:44 -0500

    Indent code.

--- app/assets/javascripts/angular/services/httpProxy.service.js
+++ app/assets/javascripts/angular/services/httpProxy.service.js
@@ -16,26 +16,26 @@
         var proxy = {
 
             callHttp: function (method, url, params, data) {
-            var task = $q.defer();
+                var task = $q.defer();
 
-            var config = {
-                method: method,
-                url: envService.read('apiUrl') + url,
-                data: data,
-                params: params
-            };
+                var config = {
+                    method: method,
+                    url: envService.read('apiUrl') + url,
+                    data: data,
+                    params: params
+                };
 
-            $http(config)
-            .then(function (response) {
-                task.resolve(response.data);
-            }, (function (error) {
-                //We can redirect to some error page if that's better
-                $log.error(error + " - Something has gone terribly wrong.");
-                task.reject();
-            }));
+                $http(config)
+                .then(function (response) {
+                    task.resolve(response.data);
+                }, (function (error) {
+                    //We can redirect to some error page if that's better
+                    $log.error(error + " - Something has gone terribly wrong.");
+                    task.reject();
+                }));
 
-            return task.promise;
-        },
+                return task.promise;
+            },
 
             get: function (url, params) {
                 return this.callHttp("GET", url, params);
