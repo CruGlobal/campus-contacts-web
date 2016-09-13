581b81477e3fb1fe8993a42ae9291c98d5baebad
Emmanuel Ijeh <emanijeh@gmail.com>
2016-09-13 09:25:01 -0500

    More tests

--- app/assets/javascripts/angular/services/test/httpProxy.service.spec.js
+++ app/assets/javascripts/angular/services/test/httpProxy.service.spec.js
@@ -7,21 +7,18 @@
 
     describe('HttpProxyService Tests', function () {
 
+
         beforeEach(angular.mock.module('ngAnimate'));
         beforeEach(angular.mock.module('ngMdIcons'));
 
-        beforeEach(angular.mock.module('missionhubApp'));
 
-        beforeEach(function(){
-            inject();
-        });
+        beforeEach(angular.mock.module('missionhubApp'));
 
         beforeEach(inject(function (_httpProxy_) {
             httpProxy = _httpProxy_;
         }));
 
         it('http proxy should exist', function() {
-
             expect(httpProxy).toBeDefined();
         });
 
