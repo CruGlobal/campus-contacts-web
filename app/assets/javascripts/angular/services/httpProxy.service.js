e5a32266af63834bd5b2bbd0444d1a3a1f2f4c32
Caleb Cox <canac@users.noreply.github.com>
2016-10-17 10:51:42 -0400

    Add a generic model loader (#768)
    * Add a generic model loading method
    
    httpProxy.getModel loads a model and its relationships while minimizing
    network traffic.
    
    * Fix typo

--- app/assets/javascripts/angular/services/httpProxy.service.js
+++ app/assets/javascripts/angular/services/httpProxy.service.js
@@ -10,7 +10,7 @@
         .factory('httpProxy', proxyService);
 
 
-    function proxyService ($http, $log, envService, JsonApiDataStore) {
+    function proxyService ($http, $log, $q, envService, JsonApiDataStore, _) {
         var proxy = {
 
             callHttp: function (method, url, params, data) {
@@ -46,6 +46,59 @@
 
             delete: function (url, params) {
                 return this.callHttp("DELETE", url, params);
+            },
+
+            // Determine whether a model has been loaded
+            isLoaded: function (model) {
+                return Boolean(model && !model._placeHolder);
+            },
+
+            // Given a model and an array of needed relationships, determine which of those relationships have not yet
+            // been loaded
+            getUnloadedRelationships: function (model, relationships) {
+                if (!proxy.isLoaded(model)) {
+                    // All relationships of unloaded models and placeholder models are considered unloaded
+                    return relationships;
+                }
+
+                // Unloaded relationships contain unloaded models
+                return relationships.filter(function (relationshipPath) {
+                    var relationshipParts = relationshipPath.split('.');
+                    var relationshipHead = _.head(relationshipParts);
+                    var relationshipTail = _.tail(relationshipParts);
+
+                    // Try to find an an unloaded model
+                    // If an unloaded model is found, this relationship will pass the filter and will be considered
+                    // to be a unloaded relationship
+                    return _.find(model[relationshipHead], function (item) {
+                        return !proxy.isLoaded(
+                            // If the relationship path consisted of multiple parts, reach into the item to pull out
+                            // the right property
+                            relationshipTail.length > 0 ? _.get(item, relationshipTail, null) : item
+                        );
+                    });
+                });
+            },
+
+            // Return a promise that resolves to the model of the specified type and with the specified id
+            // If the model is already loaded, it is returned (wrapped in a promise) without a network request
+            // If the model's required relationships are not fully loaded, those are loaded as well
+            getModel: function (url, type, id, relationships, requestParams) {
+                var model = JsonApiDataStore.store.find(type, id);
+                var unloadedRelationships = proxy.getUnloadedRelationships(model, relationships);
+                return $q.resolve().then(function () {
+                    if (unloadedRelationships.length === 0) {
+                        // All of the necessary data is already loaded
+                        return;
+                    }
+
+                    // Load the required relationships
+                    return proxy.callHttp('GET', url, _.extend({
+                        include: unloadedRelationships.join(',')
+                    }, requestParams));
+                }).then(function () {
+                    return JsonApiDataStore.store.find(type, id);
+                });
             }
         }
 
--- app/assets/javascripts/test/service/httpProxy.service.spec.js
+++ app/assets/javascripts/test/service/httpProxy.service.spec.js
@@ -4,14 +4,34 @@
 
     // Constants
     var httpProxy;
+    var JsonApiDataStore;
+    var $rootScope;
+
+    // Add better asynchronous support to a test function
+    // The test function must return a promise
+    // The promise will automatically be bound to "done" and the $rootScope will be automatically digested
+    function asynchronous (fn) {
+        return function (done) {
+            var returnValue = fn.call(this, done);
+            returnValue.then(function () {
+                done();
+            }).catch(function (err) {
+                done.fail(err);
+            });
+            $rootScope.$apply();
+            return returnValue;
+        };
+    }
 
     describe('HttpProxyService Tests', function () {
 
 
         beforeEach(angular.mock.module('missionhubApp'));
 
-        beforeEach(inject(function (_httpProxy_) {
+        beforeEach(inject(function (_httpProxy_, _$rootScope_, _JsonApiDataStore_) {
             httpProxy = _httpProxy_;
+            $rootScope = _$rootScope_;
+            JsonApiDataStore = _JsonApiDataStore_;
         }));
 
         it('service should exist', function () {
@@ -43,6 +63,118 @@
             httpProxy.callHttp();
             expect(spy).toHaveBeenCalled();
         }));
+
+        describe('model loading', function () {
+            beforeEach(function () {
+                this.loadedModel = {};
+                this.placeholderModel = { _placeHolder: true };
+            });
+
+            describe('isLoaded', function () {
+                it('should treat null models as unloaded', function () {
+                    expect(httpProxy.isLoaded(null)).toBe(false);
+                });
+
+                it('should treat a placeholder model as unloaded', function () {
+                    expect(httpProxy.isLoaded(this.placeholderModel)).toBe(false);
+                });
+
+                it('should treat a model as loaded', function () {
+                    expect(httpProxy.isLoaded(this.loadedModel)).toBe(true);
+                });
+            });
+
+            describe('getUnloadedRelationships', function () {
+                it('should treat all relationships as unloaded when the model is null', function () {
+                    this.relationships = ['a', 'b', 'c'];
+                    expect(httpProxy.getUnloadedRelationships(null, this.relationships))
+                        .toEqual(this.relationships);
+                });
+
+                it('should treat all relationships as unloaded when the model is a placeholder', function () {
+                    this.relationships = ['a', 'b', 'c'];
+                    expect(httpProxy.getUnloadedRelationships(this.placeholderModel, this.relationships))
+                        .toEqual(this.relationships);
+                });
+
+                it('should correctly detect unloaded relationships', function () {
+                    this.model = {
+                        a: [],
+                        b: [
+                            this.loadedModel
+                        ],
+                        c: [
+                            this.placeholderModel
+                        ],
+                        d: [
+                            this.loadedModel,
+                            this.placeholderModel
+                        ],
+                        e: [
+                            { a: this.loadedModel }
+                        ],
+                        f: [
+                            { a: this.placeholderModel }
+                        ],
+                        g: [
+                            { a: this.loadedModel },
+                            { a: this.placeholderModel }
+                        ],
+                        h: [
+                            { a: { b: { c: this.loadedModel } } }
+                        ],
+                        i: [
+                            { a: { b: { c: this.placeholderModel } } }
+                        ],
+                        j: [
+                            { a: this.placeholderModel }
+                        ]
+                    };
+                    this.relationships = ['a', 'b', 'c', 'd', 'e.a', 'f.a', 'g.a', 'h.a.b.c', 'i.a.b.c', 'j.a.b.c'];
+                    expect(httpProxy.getUnloadedRelationships(this.model, this.relationships))
+                        .toEqual(['c', 'd', 'f.a', 'g.a', 'i.a.b.c', 'j.a.b.c']);
+                })
+            });
+
+            describe('getModel', function () {
+                beforeEach(function () {
+                    this.url = '/';
+                    this.type = 'abc';
+                    this.id = 123;
+                    this.model = { id: this.id };
+                    this.relationships = ['a', 'b', 'c'];
+                    this.requestParams = { key: 'value' };
+
+                    spyOn(httpProxy, 'callHttp');
+                });
+
+                it('should not make a network request when the model is already loaded', asynchronous(function () {
+                    spyOn(JsonApiDataStore.store, 'find').and.returnValues(null, this.model);
+                    spyOn(httpProxy, 'getUnloadedRelationships').and.returnValue([]);
+
+                    var _this = this;
+                    return httpProxy.getModel(this.url, this.type, this.id, this.relationships).then(function (model) {
+                        expect(model).toBe(_this.model);
+                        expect(httpProxy.callHttp).not.toHaveBeenCalled();
+                    });
+                }));
+
+                it('should make a network request when the model is not already loaded', asynchronous(function () {
+                    spyOn(JsonApiDataStore.store, 'find').and.returnValues(this.model, this.model);
+                    spyOn(httpProxy, 'getUnloadedRelationships').and.returnValue(['a', 'b', 'c']);
+
+                    var _this = this;
+                    return httpProxy.getModel(this.url, this.type, this.id, this.relationships, this.requestParams)
+                        .then(function (model) {
+                            expect(model).toBe(_this.model);
+                            expect(httpProxy.callHttp).toHaveBeenCalledWith('GET', _this.url, {
+                                key: 'value',
+                                include: 'a,b,c'
+                            });
+                        });
+                }));
+            });
+        });
     });
 
 })();
