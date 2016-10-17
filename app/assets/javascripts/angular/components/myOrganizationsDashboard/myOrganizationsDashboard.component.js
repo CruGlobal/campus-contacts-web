0c5d61df9c2e1657a3f5f5a34eca543f461e98d2
Spencer Oberstadt <soberstadt@gmail.com>
2016-10-17 15:03:13 -0400

    fix my organizations dashboard (#771)
    * fix my organizations dashboard
    
    * remove myOrganizationsDashboardController 💣

--- app/assets/javascripts/angular/components/myOrganizationsDashboard/myOrganizationsDashboard.component.js
+++ app/assets/javascripts/angular/components/myOrganizationsDashboard/myOrganizationsDashboard.component.js
@@ -4,29 +4,9 @@
     angular
         .module('missionhubApp')
         .component('myOrganizationsDashboard', {
-            controller: myOrganizationsDashboardController,
             bindings: {
                 editMode: '<'
             },
             templateUrl: '/assets/angular/components/myOrganizationsDashboard/myOrganizationsDashboard.html'
         });
-
-    function myOrganizationsDashboardController (JsonApiDataStore, periodService, reportsService, _) {
-        var vm = this;
-        vm.numberOfOrgsToShow = 1000;
-
-        vm.$onInit = activate;
-
-        function activate () {
-            loadReports();
-        }
-
-        function loadReports () {
-            var organization_ids = _.map(JsonApiDataStore.store.findAll('organization'), 'id');
-            reportsService.loadOrganizationReports({
-                period: periodService.getPeriod(),
-                organization_ids: organization_ids
-            });
-        }
-    }
 })();
