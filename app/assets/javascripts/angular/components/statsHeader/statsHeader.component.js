36154c1022ffa4048468accfbc78819e7b6e7bf9
Spencer Oberstadt <soberstadt@gmail.com>
2016-10-18 08:41:48 -0400

    fix stats header (#773)

--- app/assets/javascripts/angular/components/statsHeader/statsHeader.component.js
+++ app/assets/javascripts/angular/components/statsHeader/statsHeader.component.js
@@ -4,13 +4,6 @@
     angular
         .module('missionhubApp')
         .component('statsHeader', {
-            controller: statsHeaderController,
-            bindings: {
-                title: '<'
-            },
             templateUrl: '/assets/angular/components/statsHeader/statsHeader.html'
         });
-
-    function statsHeaderController () {
-    }
 })();
