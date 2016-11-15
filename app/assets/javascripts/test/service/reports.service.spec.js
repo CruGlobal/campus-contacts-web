e576ba32bd3b88e1c37ea693dd4fa6f10f27172c
Caleb Cox <canac@users.noreply.github.com>
2016-11-15 10:47:47 -0500

    Test reportsService.loadPersonReport

--- app/assets/javascripts/test/service/reports.service.spec.js
+++ app/assets/javascripts/test/service/reports.service.spec.js
@@ -3,7 +3,7 @@
     'use strict';
 
     // Constants
-    var $q, $rootScope, reportsService, httpProxy, periodService;
+    var $q, $rootScope, reportsService, httpProxy, JsonApiDataStore, periodService;
 
     function async (fn) {
         return function (done) {
@@ -22,7 +22,8 @@
 
         beforeEach(angular.mock.module('missionhubApp'));
 
-        beforeEach(inject(function (_$q_, _$rootScope_, _reportsService_, _httpProxy_, _periodService_) {
+        beforeEach(inject(function (_$q_, _$rootScope_, _reportsService_,
+                                    _httpProxy_, _JsonApiDataStore_, _periodService_) {
 
             var _this = this;
 
@@ -30,9 +31,13 @@
             $rootScope = _$rootScope_;
             reportsService = _reportsService_;
             httpProxy = _httpProxy_;
+            JsonApiDataStore = _JsonApiDataStore_;
             periodService = _periodService_;
 
             this.period = '1d';
+            this.orgId = 1;
+            this.personId = 2;
+            this.report = { id: '1-2-1d' };
             this.orgIds = [123, 456, 789];
             this.report1 = { id: '123-1d' };
             this.report2 = { id: '456-1d' };
@@ -46,15 +51,64 @@
                 ]
             };
 
-            spyOn(httpProxy, 'callHttp').and.returnValue($q.resolve());
+            this.httpResponse = {};
+            spyOn(httpProxy, 'callHttp').and.callFake(function () {
+                return $q.resolve(_this.httpResponse);
+            });
+            spyOn(periodService, 'getPeriod').and.returnValue(this.period);
         }));
 
-
-        describe('loadOrganizationReports', function () {
-            beforeEach(function () {
-                spyOn(periodService, 'getPeriod').and.returnValue(this.period);
+        describe('loadPersonReport', function () {
+            it('should try to find the existing person report', function () {
+                spyOn(JsonApiDataStore.store, 'find');
+                reportsService.loadPersonReport(this.orgId, this.personId);
+                expect(JsonApiDataStore.store.find).toHaveBeenCalledWith(
+                    'person_report',
+                    this.orgId + '-' + this.personId + '-' + this.period
+                );
             });
 
+            it('should make a network request if the report is not loaded', function () {
+                spyOn(JsonApiDataStore.store, 'find').and.returnValue(null);
+                reportsService.loadPersonReport(this.orgId, this.personId);
+                expect(httpProxy.callHttp).toHaveBeenCalledWith(
+                    'GET',
+                    jasmine.any(String),
+                    {
+                        period: this.period,
+                        organization_ids: this.orgId.toString(),
+                        people_ids: this.personId.toString()
+                    }
+                );
+            });
+
+            it('should not make a network request if the report is loaded', function () {
+                spyOn(JsonApiDataStore.store, 'find').and.returnValue(this.report);
+                reportsService.loadPersonReport(this.orgId, this.personId);
+                expect(httpProxy.callHttp).not.toHaveBeenCalled();
+            });
+
+            it('should return a promise that resolves to the report if it is already loaded', async(function () {
+                var _this = this;
+                spyOn(JsonApiDataStore.store, 'find').and.returnValue(this.report);
+                return reportsService.loadPersonReport(this.orgId, this.personId).then(function (personReport) {
+                    expect(personReport).toBe(_this.report);
+                });
+            }));
+
+            it('should return a promise that resolves to the report if it is not already loaded', async(function () {
+                var _this = this;
+                spyOn(JsonApiDataStore.store, 'find').and.returnValue(null);
+                this.httpResponse = {
+                    data: [this.report]
+                };
+                return reportsService.loadPersonReport(this.orgId, this.personId).then(function (personReport) {
+                    expect(personReport).toBe(_this.report);
+                });
+            }));
+        });
+
+        describe('loadOrganizationReports', function () {
             it('should GET the organization reports URL', function () {
                 spyOn(reportsService, 'lookupOrganizationReport').and.returnValues(
                     null, null, null
