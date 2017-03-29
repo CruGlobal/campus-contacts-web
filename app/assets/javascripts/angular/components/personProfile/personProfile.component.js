CruGlobal/campus-contacts-web

commit e8b9e425a1bdac4017acaf42454dde64c9a940c6

Stop referring to people as contacts (#851)
* Stop referring to people as contacts

* Undo i18n label replacement

* Update styles to match renamed components

* Undo more i18n label replacements

app/assets/javascripts/angular/components/personProfile/personProfile.component.js
@@ -1,0 +1,190 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('missionhubApp')
+        .component('personProfile', {
+            controller: personProfileController,
+            require: {
+                personTab: '^personPage'
+            },
+            templateUrl: '/assets/angular/components/personProfile/personProfile.html'
+        });
+
+    function personProfileController ($scope, $filter, JsonApiDataStore, jQuery,
+                                       personProfileService, loggedInPerson, _) {
+        var vm = this;
+
+        vm.pendingEmailAddress = null;
+        vm.pendingPhoneNumber = null;
+
+        vm.saveAttribute = saveAttribute;
+        vm.getEmailAddresses = getEmailAddresses;
+        vm.getPhoneNumbers = getPhoneNumbers;
+        vm.addEmailAddress = addEmailAddress;
+        vm.addPhoneNumber = addPhoneNumber;
+        vm.deleteEmailAddress = deleteEmailAddress;
+        vm.deletePhoneNumber = deletePhoneNumber;
+        vm.permissionChange = permissionChange;
+
+        vm.$onInit = activate;
+
+        // Each of these arrays contains all possible values for a partiuclar person attribute
+        // The ids also match the last part of the i18n label path (i.e., 'cru_status.{none,volunteer,affiliate,...}')
+        vm.followupStatusIds = [
+            'attempted_contact',
+            'completed',
+            'contacted',
+            'do_not_contact',
+            'uncontacted'
+        ];
+        vm.cruStatusIds = [
+            'none',
+            'volunteer',
+            'affiliate',
+            'intern',
+            'part_time_staff',
+            'full_time_staff'
+        ];
+        vm.permissionOptions = [
+            { id: 1, i18n: 'admin' },
+            { id: 4, i18n: 'user' },
+            { id: 2, i18n: 'no_permissions' }
+        ];
+        vm.enrollmentIds = [
+            'not_student',
+            'middle_school',
+            'high_school',
+            'collegiate',
+            'masters_or_doctorate'
+        ];
+
+        function activate () {
+            var organization = JsonApiDataStore.store.find('organization', vm.personTab.organizationId);
+            if (loggedInPerson.person === vm.personTab.person) {
+                vm.permissionChangeDisabled = true;
+            } else if (!loggedInPerson.isAdminAt(organization)) {
+                if (vm.personTab.orgPermission.permission_id === 1) {
+                    vm.permissionChangeDisabled = true;
+                } else {
+                    vm.permissionOptions.shift();
+                }
+            }
+
+            // Save the changes on the server whenever the primary email or primary phone changes
+            $scope.$watch('$ctrl.personTab.primaryEmail', updatePrimary);
+            $scope.$watch('$ctrl.personTab.primaryPhone', updatePrimary);
+
+            // Whenever the person has no email addresses or phone numbers, show the add email address or add phone
+            // number form
+            $scope.$watch('$ctrl.personTab.person.email_addresses.length', function (newLength) {
+                if (newLength === 0) {
+                    vm.addEmailAddress();
+                }
+            });
+            $scope.$watch('$ctrl.personTab.person.phone_numbers.length', function (newLength) {
+                if (newLength === 0) {
+                    vm.addPhoneNumber();
+                }
+            });
+
+            $scope.$watchCollection('$ctrl.personTab.assignedTo', function (newAssignedTo, oldAssignedTo) {
+                var addedPeople = _.difference(newAssignedTo, oldAssignedTo);
+                personProfileService.addAssignments(vm.personTab.person, vm.personTab.organizationId, addedPeople);
+
+                var removedPeople = _.difference(oldAssignedTo, newAssignedTo);
+                personProfileService.removeAssignments(vm.personTab.person, removedPeople);
+            });
+
+            $scope.$watchCollection('$ctrl.personTab.person.gender', function (newGender, oldGender) {
+                if (newGender !== oldGender) {
+                    saveAttribute(vm.personTab.person, 'gender');
+                }
+            });
+        }
+
+        function updatePrimary (newPrimary, oldPrimary) {
+            if (newPrimary === oldPrimary) {
+                // Do nothing if the primary item is not changing
+                return;
+            }
+
+            if (newPrimary && !newPrimary.primary) {
+                newPrimary.primary = true;
+                saveAttribute(newPrimary, 'primary');
+            }
+
+            if (oldPrimary) {
+                oldPrimary.primary = false;
+            }
+        }
+
+        function saveAttribute (model, attribute) {
+            personProfileService.saveAttribute(vm.personTab.person.id, model, attribute).then(function () {
+                if (model === vm.pendingEmailAddress) {
+                    vm.pendingEmailAddress = null;
+                } else if (model === vm.pendingPhoneNumber) {
+                    vm.pendingPhoneNumber = null;
+                }
+            });
+        }
+
+        function getEmailAddresses () {
+            var emailAddresses = vm.personTab.person.email_addresses;
+            return vm.pendingEmailAddress ? emailAddresses.concat(vm.pendingEmailAddress) : emailAddresses;
+        }
+
+        function getPhoneNumbers () {
+            var phoneNumbers = vm.personTab.person.phone_numbers;
+            return vm.pendingPhoneNumber ? phoneNumbers.concat(vm.pendingPhoneNumber) : phoneNumbers;
+        }
+
+        // Add a new email address to the person
+        // The email address is not actually saved to the server until the call to saveAttribute
+        function addEmailAddress () {
+            vm.pendingEmailAddress = {
+                _type: 'email_address',
+                person_id: vm.personTab.person.id
+            };
+        }
+
+        // Add a new phone number to the person
+        // The phone number is not actually saved to the server until the call to saveAttribute
+        function addPhoneNumber () {
+            vm.pendingPhoneNumber = {
+                _type: 'phone_number',
+                person_id: vm.personTab.person.id
+            };
+        }
+
+        function deleteEmailAddress (emailAddress) {
+            return personProfileService.deleteModel(emailAddress).then(function () {
+                // Remove the deleted email address
+                _.pull(vm.personTab.person.email_addresses, emailAddress);
+            });
+        }
+
+        function deletePhoneNumber (phoneNumber) {
+            return personProfileService.deleteModel(phoneNumber).then(function () {
+                // Remove the deleted phone number
+                _.pull(vm.personTab.person.phone_numbers, phoneNumber);
+            });
+        }
+
+        /*
+        This is used on the profile component with a tricky interpolation that results in it
+        returning the old value. It looks like this:
+        ng-change="$ctrl.permissionChange({{$ctrl.personTab.orgPermission.permission_id}})"
+        Reference: http://stackoverflow.com/a/28047112/879524
+         */
+        function permissionChange (oldValue) {
+            var hasEmailAddress = vm.personTab.person.email_addresses.length > 0;
+            if (hasEmailAddress) {
+                vm.saveAttribute(vm.personTab.orgPermission, 'permission_id');
+            } else {
+                vm.personTab.orgPermission.permission_id = oldValue;
+                jQuery.a($filter('t')('people.index.for_this_permission_email_is_required_no_name'));
+            }
+        }
+    }
+})();

commit 4b18c0bf56ad4a0d96fc0fd538033bf238faaef7

Fix i18n label accidentally broken in #851 (#914)

app/assets/javascripts/angular/components/personProfile/personProfile.component.js
@@ -198,7 +198,7 @@
                 vm.saveAttribute(vm.personTab.orgPermission, 'permission_id');
             } else {
                 vm.personTab.orgPermission.permission_id = oldValue;
-                jQuery.a($filter('t')('people.index.for_this_permission_email_is_required_no_name'));
+                jQuery.a($filter('t')('contacts.index.for_this_permission_email_is_required_no_name'));
             }
         }
 