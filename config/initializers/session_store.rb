15f361ba13018d3a54dea949846c53eab3bec674
Spencer Oberstadt <soberstadt@gmail.com>
2017-04-11 13:24:11 -0400

    set TTL on redis sessions (#925)
    * set TTL on redis sessions
    
    * extend session length
    
    * clean up code style

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,9 @@
 require Rails.root.join('config', 'initializers', 'redis')
 
-Rails.application.config.session_store :redis_store, servers: {
+server_config = {
   host: Redis.current.client.host,
   port: Redis.current.client.port,
   db: 2,
-  namespace: 'missionhub:session:',
-  expires_in: 2.days
+  namespace: 'missionhub:session:'
 }
+Rails.application.config.session_store :redis_store, servers: server_config, expire_after: 30.days
