b6cad9072df10b6740fef9cf9726467c23c623d4
Caleb Cox <canac@users.noreply.github.com>
2017-04-17 11:44:35 -0400

    Ignore missing name parts (#933)
    Previously, placeholder people were being rendered as "undefined undefined".

--- app/assets/javascripts/angular/filters/personName.filter.js
+++ app/assets/javascripts/angular/filters/personName.filter.js
@@ -5,9 +5,14 @@
      * The personName filter takes a perosn and converts it into the person's name.
      */
     angular.module('missionhubApp')
-        .filter('personName', function () {
+        .filter('personName', function (_) {
             return function (person) {
-                return person ? person.first_name + ' ' + person.last_name : null;
+                if (!person) {
+                    return null;
+                }
+
+                // Ignore missing name parts
+                return [person.first_name, person.last_name].filter(_.identity).join(' ');
             };
         });
 })();
