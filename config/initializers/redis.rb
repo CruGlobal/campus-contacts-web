ca2b51dde2c573dc33a9c410995715047d633968
Josh Starcher <josh.starcher@gmail.com>
2017-04-24 12:27:05 -0400

    Set defaults for some env vars

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,4 @@
-
 require 'redis'
 require 'redis/namespace'
 
-redis_config = YAML.load(ERB.new(File.read(Rails.root.join('config', 'redis.yml').to_s)).result)
-host, port = redis_config[Rails.env].split(':')
-Redis.current = Redis.new(host: host, port: port)
+Redis.current = Redis.new(host: ENV['REDIS_PORT_6379_TCP_ADDR'])
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   user_name: ENV.fetch('SMTP_USER_NAME'),
   password: ENV.fetch('SMTP_PASSWORD'),
   address: ENV.fetch('SMTP_ADDRESS'),
-  authentication: (ENV.fetch('SMTP_AUTHENTICATION') || :none),
-  enable_starttls_auto: ENV.fetch('SMTP_ENABLE_STARTTLS_AUTO'),
-  port: ENV.fetch('SMTP_PORT') || 25
+  authentication: ENV['SMTP_AUTHENTICATION'] || :none,
+  enable_starttls_auto: ENV['SMTP_ENABLE_STARTTLS_AUTO'] || true,
+  port: ENV['SMTP_PORT'] || 587
 }
