1c924ebba3f19ea706cccfee028bd4ee99e4b5b9
Spencer Oberstadt <soberstadt@gmail.com>
2017-05-16 09:56:49 -0400

    fix bootstrap modals on legacy pages (#980)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,5 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( jquery-1.7.2.min.js jquery-ui-1.8.12.js welcome.css bootstrap.js qe/icons/* highcharts.js highcharts-more.js no_left_sidebar.css plain.css)
 Rails.application.config.assets.precompile += %w( welcome.js )
 Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( legacy_bootstrap.css )
 Rails.application.config.assets.precompile += %w( country-region-data/data.json )
