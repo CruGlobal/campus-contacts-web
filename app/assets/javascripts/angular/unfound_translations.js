cfceec6bc69434415257af7555011318a3b6cc47
Spencer Oberstadt <soberstadt@gmail.com>
2017-05-17 09:57:59 -0400

    add translation tools (#981)
    * add translation tools
    
    * add more unfound translation keys
    
    * disable the cop

---
+++ app/assets/javascripts/angular/unfound_translations.js
@@ -0,0 +1,37 @@
+// translation strings here are marked as being used by the Angular app.
+
+// nav.contacts
+// contacts.index.search_locate_contact_button
+// contacts.index.search_contact_results_description
+// dashboard.people_assigned_to_you
+// dashboard.organizations_assigned_to_you
+// groups.edit.edit_group
+
+// ministries.suborgs.header
+// ministries.team.header
+// ministries.groups.header
+// ministries.people.profile.header
+// ministries.people.history.header
+// ministries.people.history.all
+// ministries.people.history.anonymous
+// ministries.people.history.interactions
+// ministries.people.history.me
+// ministries.people.history.note_for_interaction
+// ministries.people.history.notes
+// ministries.people.history.surveys
+// ministries.people.assigned.header
+// ministries.people.activity.header
+// ministries.people.male
+// ministries.people.female
+
+// address_types.current
+// address_types.permanent
+// address_types.emergency1
+// address_types.emergency2
+
+// followup_status.attempted_contact
+// followup_status.completed
+// followup_status.contacted
+// followup_status.do_not_contact
+// followup_status.uncontacted
+
