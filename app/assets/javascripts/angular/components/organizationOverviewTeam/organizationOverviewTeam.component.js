2a7d5c088cc5a909e2b1a5adea4f33656bdc5b16
Scotty Waggoner <ozzieorca@gmail.com>
2017-09-28 10:19:23 -0700

    MHP-903: Load team reports

--- app/assets/javascripts/angular/components/organizationOverviewTeam/organizationOverviewTeam.component.js
+++ app/assets/javascripts/angular/components/organizationOverviewTeam/organizationOverviewTeam.component.js
@@ -13,7 +13,8 @@
             }
         });
 
-    function organizationOverviewTeamController (organizationOverviewTeamService, ProgressiveListLoader) {
+    function organizationOverviewTeamController ($log, _, organizationOverviewTeamService, ProgressiveListLoader,
+                                                 myPeopleDashboardService) {
         var vm = this;
         vm.team = [];
         vm.loadTeamPage = loadTeamPage;
@@ -32,10 +33,25 @@
             return organizationOverviewTeamService.loadOrgTeam(vm.organizationOverview.org, listLoader)
                 .then(function (resp) {
                     vm.team = resp.list;
+                    loadReports(vm.team);
                     vm.loadedAll = resp.loadedAll;
                 })
                 .finally(function () {
                     vm.busy = false;
+                });
+        }
+
+        function loadReports (team) {
+            var peopleIds = _.map(team, 'id');
+
+            var peopleReportParams = {
+                organization_ids: [vm.organizationOverview.org.id],
+                people_ids: peopleIds
+            };
+
+            myPeopleDashboardService.loadPeopleReports(peopleReportParams)
+                .catch(function (error) {
+                    $log.error('Error loading team people reports', error);
                 });
         }
     }
