a7f89bce21e3ea9a1450e1406a4483bbb9d2a524
Scotty Waggoner <ozzieorca@gmail.com>
2018-05-29 18:38:22 -0700

    Upgrade packages
    - angular.js to 1.7
    - webpack to v4

--- app/assets/javascripts/angular/services/geoData.service.js
+++ app/assets/javascripts/angular/services/geoData.service.js
@@ -7,7 +7,7 @@ function geoDataService (_) {
         // Return a promise that resolves to the countries list
         getCountries: function () {
             return import(/* webpackChunkName: "country-region-data" */ 'country-region-data')
-                .then(function (countries) {
+                .then(function ({ default: countries }) {
                     // Manipulate the data schema
                     return countries.map(function (country) {
                         // Rename the country name and short code fields
