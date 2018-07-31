8ca084161c22208c7f7b782aa187b717ff39192b
Scotty Waggoner <ozzieorca@gmail.com>
2018-07-30 19:23:56 -0700

    Extract listLoader and buildListParams to parent component

--- app/assets/javascripts/angular/components/organizationOverviewPeople/organizationOverviewPeople.component.js
+++ app/assets/javascripts/angular/components/organizationOverviewPeople/organizationOverviewPeople.component.js
@@ -1,8 +1,15 @@
 import template from './organizationOverviewPeople.html';
 
 angular.module('missionhubApp').component('organizationOverviewPeople', {
+    controller: organizationOverviewPeopleController,
     require: {
         organizationOverview: '^',
     },
     template: template,
 });
+function organizationOverviewPeopleController(
+    organizationOverviewPeopleService,
+) {
+    this.listLoader = organizationOverviewPeopleService.listLoader;
+    this.buildListParams = organizationOverviewPeopleService.buildListParams;
+}
