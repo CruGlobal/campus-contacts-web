7b9aa72d0b63155f95a5cf837f32da50958d5d82
Scotty Waggoner <ozzieorca@gmail.com>
2018-08-15 16:34:13 -0700

    Add survey filters
    - Remove clientside question sort since API does it
    - Make accordion handle header toggles internally

--- src/i18n/locales/en-US.js
+++ src/i18n/locales/en-US.js
@@ -612,6 +612,9 @@ export default {
             gender: {
                 title: 'Gender',
             },
+            questions: {
+                title: 'Survey Questions',
+            },
             sidebar: {
                 include_archived: 'Include archived contacts',
             },
