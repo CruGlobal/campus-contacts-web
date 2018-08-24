1f568fff586643b5a93e2773b6333bc42440a01c
Scotty Waggoner <ozzieorca@gmail.com>
2018-08-24 12:27:58 -0700

    Fix updating people in people screen

--- app/assets/javascripts/angular/components/peopleScreen/peopleScreen.service.js
+++ app/assets/javascripts/angular/components/peopleScreen/peopleScreen.service.js
@@ -178,8 +178,8 @@ function peopleScreenService(
         },
 
         transformData: params => data => {
-            return {
-                ...data,
+            // We MUST mutate this object here to keep Angular watchers watching the JsonApiDataStore for changes which is stupid. Everything should work with immutable data but it wasn't implemented that way... An example where creating a new object broke something is updating a contact assignment on the peopleScreen.
+            return Object.assign(data, {
                 answers: (
                     _.findLast(
                         data.answer_sheets,
@@ -197,7 +197,7 @@ function peopleScreenService(
                     }),
                     {},
                 ),
-            };
+            });
         },
 
         // Merge the specified people
