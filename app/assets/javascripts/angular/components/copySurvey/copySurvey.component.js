3a463086196e3c8374a7fc9c6c6e0213ca17118a
Adam Meyer <adam.meyer@cru.org>
2018-08-27 09:12:12 -0400

    only show surveys that user has admin permissions to

--- app/assets/javascripts/angular/components/copySurvey/copySurvey.component.js
+++ app/assets/javascripts/angular/components/copySurvey/copySurvey.component.js
@@ -21,6 +21,9 @@ function copySurveyController(surveyService, loggedInPerson) {
         this.orgs = loggedInPerson.person.organizational_permissions.map(
             orgPermission => orgPermission.organization,
         );
+        this.orgs = this.orgs.filter(org => {
+            return loggedInPerson.isAdminAt(org);
+        });
 
         //default to current org
         this.survey.parent_organization = this.resolve.organizationId;
