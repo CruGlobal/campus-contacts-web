df38f9220bc935b40dc492d833a4de44a60e1e6f
Adam Meyer <adam.meyer@cru.org>
2018-08-31 10:52:21 -0400

    allow options reassign

--- app/assets/javascripts/angular/services/confirmModal.service.js
+++ app/assets/javascripts/angular/services/confirmModal.service.js
@@ -7,6 +7,7 @@ angular
 function confirmModalService($uibModal) {
     return {
         create: function(message, options) {
+            /*eslint no-param-reassign: 0*/
             options = _.defaults(options, {
                 showCancel: true,
                 title: 'Confirm',
