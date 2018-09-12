9495f7b3ff8b291c6cc9a124e94a850ec8b861b8
Scotty Waggoner <ozzieorca@gmail.com>
2018-09-12 15:22:33 -0700

    Fix reset filters

--- app/assets/javascripts/angular/components/peopleFiltersPanel/peopleFiltersPanel.service.js
+++ app/assets/javascripts/angular/components/peopleFiltersPanel/peopleFiltersPanel.service.js
@@ -1,8 +1,10 @@
+import _ from 'lodash';
+
 angular
     .module('missionhubApp')
     .factory('peopleFiltersPanelService', peopleFiltersPanelService);
 
-function peopleFiltersPanelService(_) {
+function peopleFiltersPanelService() {
     return {
         // Determine whether a filters object contains any active filters or not
         filtersHasActive: function(filters) {
@@ -11,7 +13,11 @@ function peopleFiltersPanelService(_) {
                 filters.includeArchived === true ||
                 _.keys(filters.labels).length > 0 ||
                 _.keys(filters.assignedTos).length > 0 ||
-                _.keys(filters.groups).length > 0
+                _.keys(filters.groups).length > 0 ||
+                _.keys(filters.statuses).length > 0 ||
+                _.keys(filters.genders).length > 0 ||
+                _.keys(filters.questions).length > 0 ||
+                filters.includeArchived === true
             );
         },
     };
