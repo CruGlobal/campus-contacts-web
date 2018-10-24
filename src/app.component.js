077e6824dd35988de800cf11d6ce3c43fcf9dbee
Jon Warmington <jon@japache.ca>
2018-10-24 16:40:31 -0400

    add new app components

---
+++ src/app.component.js
@@ -0,0 +1,64 @@
+import template from './app.html';
+
+angular.module('missionhubApp').component('app', {
+    controller: appController,
+    template: template,
+    bindings: {
+        jwtToken: '@',
+        access: '<',
+        currentOrganization: '<',
+        branded: '<',
+    },
+});
+
+function appController(
+    loggedInPerson,
+    periodService,
+    $uibModal,
+    $rootScope,
+    state,
+    $http,
+) {
+    let deregisterEditOrganizationsEvent;
+
+    this.editOrganizations = false;
+    this.getPeriod = periodService.getPeriod;
+    this.loggedInPerson = loggedInPerson;
+
+    const setAuthorizationAndState = () => {
+        state.branded = this.branded;
+        state.hasMissionhubAccess = this.access;
+        state.currentOrganization = this.currentOrganization
+            ? this.currentOrganization
+            : 0;
+
+        if (this.jwtToken) {
+            state.v4AccessToken = this.jwtToken;
+            $http.defaults.headers.common.Authorization =
+                'Bearer ' + this.jwtToken;
+        }
+    };
+
+    this.$onInit = async () => {
+        setAuthorizationAndState();
+        deregisterEditOrganizationsEvent = $rootScope.$on(
+            'editOrganizations',
+            function(event, value) {
+                this.editOrganizations = value;
+            },
+        );
+
+        const { user } = await loggedInPerson.loadOnce();
+
+        if (user.beta_mode === null) {
+            $uibModal.open({
+                component: 'newWelcomeModal',
+                resolve: {},
+                windowClass: 'pivot_theme',
+                size: 'sm',
+            });
+        }
+    };
+
+    this.$onDestroy = () => deregisterEditOrganizationsEvent();
+}
