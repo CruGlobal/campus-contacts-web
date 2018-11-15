b5fa519f68e6dd30d24f50779893fde91e2fc118
Jon Warmington <jon@japache.ca>
2018-11-15 14:56:04 -0500

    update login to handel auth return from the key, and update to pass new key url

--- src/components/authentication/login.component.js
+++ src/components/authentication/login.component.js
@@ -4,10 +4,22 @@ import './login.scss';
 angular.module('missionhubApp').component('login', {
     controller: loginController,
     template: template,
+    bindings: {
+        accessToken: '<',
+    },
 });
 
-function loginController(envService) {
-    this.$onInit = () => {
+function loginController(authenticationService, envService, $state) {
+    this.$onInit = async () => {
+        this.theKeyUrl = authenticationService.theKeyloginUrl;
         this.url = envService.read('railsUrl');
+
+        if (authenticationService.isTokenValid()) {
+            $state.go('app.people');
+        }
+
+        if (this.accessToken) {
+            await authenticationService.authorizeAccess(this.accessToken);
+        }
     };
 }
