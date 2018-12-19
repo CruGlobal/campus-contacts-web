1cb8b9028409843a2a4992e705b781af2886cdc6
Jon Warmington <jon@japache.ca>
2018-12-19 17:12:07 -0500

    start adding logic to archive contacts

--- app/assets/javascripts/angular/components/organizationCleanup/organizationCleanup.component.js
+++ app/assets/javascripts/angular/components/organizationCleanup/organizationCleanup.component.js
@@ -9,12 +9,41 @@ angular.module('missionhubApp').component('organizationCleanup', {
     controller: organizationCleanupController,
 });
 
-function organizationCleanupController() {
-    this.archiveDate = new Date();
+function organizationCleanupController(httpProxy) {
+    this.archiveBeforeDate = new Date();
+    this.archiveInactivityDate = new Date();
     this.dateOptions = {
         formatYear: 'yy',
         maxDate: new Date(2020, 5, 22),
         minDate: new Date(),
         startingDay: 1,
+    };
+
+    const archiveContacts = async (archiveBy, dateBy) => {
+        const archiveDate = new Date();
+
+        return await httpProxy.post(
+            '/organizations/archives',
+            {
+                type: 'bulk_archive_contacts',
+                id: this.orgId,
+                'attributes[organization_id]': this.orgId,
+                'attributes[before_date]': dateBy.string(),
+                'attributes[archive_by]': archiveBy,
+                'attributes[archived]': true,
+                'attributes[archive_date]': archiveDate.string(),
+            },
+            {
+                errorMessage: 'error.messages.surveys.loadQuestions',
+            },
+        );
+    };
+
+    this.archiveInactive = async dateBy => {
+        const { data } = archiveContacts('persons_inactive_since', dateBy);
+    };
+
+    this.archiveBefore = async dateBy => {
+        const { data } = archiveContacts('persons_inactive_since', dateBy);
     };
 }
