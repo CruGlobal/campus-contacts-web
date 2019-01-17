03954a873a72b699bc069d090a8c827428efe4cc
Jon Warmington <jon@japache.ca>
2019-01-17 09:20:24 -0500

    remove template for pagination, its no longer being used

--- app/assets/javascripts/angular/missionhubApp.run.js
+++ app/assets/javascripts/angular/missionhubApp.run.js
@@ -42,22 +42,4 @@ angular
                 });
             }
         });
-    })
-    .run(function($templateCache) {
-        $templateCache.put(
-            'template/smart-table/pagination.html',
-            `<div class="pagination" ng-if="pages.length >= 2">{{currentPage}}
-                <ul class="pagination list flex">
-                    <li ng-if="1 !== pages[0]">
-                        <a ng-click="selectPage(1)">1...</a>
-                     </li>
-                    <li ng-repeat="page in pages" class="ml2" ng-class="{active: page==currentPage}">
-                        <a ng-click="selectPage(page);">{{page}}</a>
-                    </li>
-                    <li ng-if="numPages !== pages[pages.length - 1]">
-                        <a ng-click="selectPage(numPages)">...{{numPages}}</a>
-                    </li>
-                </ul>
-            </div>`,
-        );
     });
