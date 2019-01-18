841a037678e5a0bf595dfae2b61b46931fa4b8ac
Tataihono Nikora <tataihono.nikora@gmail.com>
2019-01-18 13:39:14 +1300

    api changes

--- app/assets/javascripts/angular/components/peopleScreen/peopleScreen.service.js
+++ app/assets/javascripts/angular/components/peopleScreen/peopleScreen.service.js
@@ -171,7 +171,7 @@ function peopleScreenService(
             'fields[organizational_permission]':
                 'followup_status,organization_id,permission_id',
             'fields[email_address]': 'email,primary',
-            'fields[phone_number]': 'number,primary,valid_values',
+            'fields[phone_number]': 'number,primary,validated',
             'fields[contact_assignment]': 'assigned_to,organization,created_at',
             'fields[answer_sheet]': 'answers,survey',
             'fields[answer]': 'value,question',
