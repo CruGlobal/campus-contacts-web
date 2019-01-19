27689b71b0b3e8e12b06ed9b851c7fceb398e858
Tataihono Nikora <tataihono.nikora@gmail.com>
2019-01-19 14:11:24 +1300

    phone number validation (#95)
    * update to support new api
    
    * update phone number validation
    
    * add validation check mark
    
    * update routes and payload
    
    * update survey overview
    
    * update message
    
    * use disabled instead of readonly
    
    * move survey options around
    
    * update to show logo
    
    * update service to retrive validation status of phone number
    
    * update styles
    
    * api changes
    
    * tidy up styles

--- app/assets/javascripts/angular/services/routes.service.js
+++ app/assets/javascripts/angular/services/routes.service.js
@@ -71,5 +71,20 @@ function routesService(httpProxy, modelsService) {
                 },
             );
         },
+
+        getPhoneNumberValidation: function(code, id) {
+            return httpProxy.getModel(
+                modelsService
+                    .getModelMetadata('phone_number_validation')
+                    .url.single(code),
+                'phone_number_validation',
+                id,
+                ['survey', 'phone_number'],
+                {
+                    errorMessage:
+                        'error.messages.routes.get_phone_number_validation',
+                },
+            );
+        },
     };
 }
