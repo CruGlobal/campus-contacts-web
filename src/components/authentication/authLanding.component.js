62b0365b1ecd194a33cfd734a2ebc3411808430f
Jon Warmington <jon@japache.ca>
2019-02-28 13:37:15 -0500

    remove unused logic in auth landing

--- src/components/authentication/authLanding.component.js
+++ src/components/authentication/authLanding.component.js
@@ -1,11 +1,8 @@
 import template from './authLanding.html';
 
 angular.module('missionhubApp').component('authLanding', {
-    controller: authLandingController,
     template: template,
     bindings: {
         accessToken: '<',
     },
 });
-
-function authLandingController() {}
