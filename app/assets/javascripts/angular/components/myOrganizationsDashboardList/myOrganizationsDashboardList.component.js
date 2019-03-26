57772db2c77f5e35d301a86a0e4f373e984ea629
Jon Warmington <jon@japache.ca>
2019-03-26 11:46:44 -0400

    add catch broadcast to my org fashboard list of editOrgs

--- app/assets/javascripts/angular/components/myOrganizationsDashboardList/myOrganizationsDashboardList.component.js
+++ app/assets/javascripts/angular/components/myOrganizationsDashboardList/myOrganizationsDashboardList.component.js
@@ -8,8 +8,22 @@ angular.module('missionhubApp').component('myOrganizationsDashboardList', {
     bindings: {
         rootOrgs: '<',
     },
-    controller: /* @ngInject */ function(userPreferencesService) {
+    controller: /* @ngInject */ function(userPreferencesService, $scope) {
+        let deregisterEditOrganizationsEvent;
+
+        deregisterEditOrganizationsEvent = $scope.$on(
+            'editOrganizations',
+            (event, value) => {
+                console.log(value);
+                this.editOrganizations = value;
+            },
+        );
+
         var vm = this;
+
+        this.$onDestroy = () => {
+            deregisterEditOrganizationsEvent();
+        };
 
         vm.sortableOptions = {
             handle: '.sort-orgs-handle',
