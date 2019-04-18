8f50440e7a863a9f3a3c562d70f44b6e511877ed
Bartosz Wilczyński <>
2019-04-18 13:18:30 +0200

    MHP-2497: Improving sessions handling between tabs

--- app/assets/javascripts/angular/services/authentication.service.js
+++ app/assets/javascripts/angular/services/authentication.service.js
@@ -37,20 +37,12 @@ function authenticationService(
     )}&service=https://${$location.host()}${port}/sign-in`;
 
     const getJwtToken = () => {
-        return (
-            (envService.is('development')
-                ? localStorageService.get('jwtToken')
-                : sessionStorageService.get('jwtToken')) || false
-        );
+        return localStorageService.get('jwtToken') || false;
     };
 
     const updateUserData = async () => {
         const me = await loggedInPerson.load();
-
-        const currentState = envService.is('development')
-            ? localStorageService.get('state')
-            : sessionStorageService.get('state');
-
+        const currentState = localStorageService.get('state');
         const currentOrganization = currentState
             ? currentState.currentOrganization
             : null;
@@ -73,7 +65,6 @@ function authenticationService(
 
     const clearToken = () => {
         state.v4AccessToken = null;
-        sessionStorageService.destroy('jwtToken');
         localStorageService.destroy('jwtToken');
         $http.defaults.headers.common.Authorization = null;
     };
@@ -82,10 +73,7 @@ function authenticationService(
         if (!token) return;
 
         state.v4AccessToken = token;
-        sessionStorageService.set('jwtToken', token);
-
-        if (envService.is('development'))
-            localStorageService.set('jwtToken', token);
+        localStorageService.set('jwtToken', token);
     };
 
     const setAuthorizationAndState = (token, organization) => {
@@ -144,9 +132,7 @@ function authenticationService(
     };
 
     const loadState = () => {
-        const currentState = envService.is('development')
-            ? localStorageService.get('state')
-            : sessionStorageService.get('state');
+        const currentState = localStorageService.get('state');
 
         if (!currentState) return;
 
@@ -168,16 +154,12 @@ function authenticationService(
                 : [],
         };
 
-        sessionStorageService.set('state', newState);
-
-        if (envService.is('development'))
-            localStorageService.set('state', newState);
+        localStorageService.set('state', newState);
 
         loadState();
     };
 
     const clearState = () => {
-        sessionStorageService.destroy('state');
         localStorageService.destroy('state');
         state.hasMissionhubAccess = null;
         state.currentOrganization = null;
