2c1b71f89e69a8fc699e7d623f7b6685770eff30
bwtoptal <bartosz.w@toptal.com>
2019-06-06 15:02:26 +0200

    MHP-2482: Fixing tests on Travis

--- apollo.config.js
+++ apollo.config.js
@@ -1,15 +1,8 @@
-const prod = process.env.TRAVIS_BRANCH === 'master';
-
 module.exports = {
     client: {
-        service: prod
-            ? {
-                  name: 'production',
-                  url: 'https://api.missionhub.com/apis/graphql',
-              }
-            : {
-                  name: 'staging',
-                  url: 'https://api-stage.missionhub.com/apis/graphql',
-              },
+        service: {
+            name: 'staging',
+            url: 'https://api-stage.missionhub.com/apis/graphql',
+        },
     },
 };
