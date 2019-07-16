56ba9010bfba9152744ae3cf86ee52a86ec4a16a
TheNoodleMoose <cjh605@gmail.com>
2019-07-16 13:39:10 -0400

    Added confirmation modal for archiving by date and inactivity, also changed styling so edit icon is not invivisble on manage labels page

--- app/assets/javascripts/angular/components/organizationCleanup/organizationCleanup.component.js
+++ app/assets/javascripts/angular/components/organizationCleanup/organizationCleanup.component.js
@@ -1,5 +1,6 @@
 import './organizationCleanup.scss';
 import checkIcon from '../../../../images/icons/icon-check.svg';
+import warningIcon from '../../../../images/icons/icon-warning-2.svg';
 
 import template from './organizationCleanup.html';
 
@@ -21,10 +22,80 @@ function organizationCleanupController(
     this.archiveBeforeDate = new Date();
     this.archiveInactivityDate = new Date();
     this.checkIcon = checkIcon;
+    this.warningIcon = warningIcon;
+
+    // Opens up modal depending on the type
+    const confirmModal = async (type, archiveDate) => {
+        switch (type) {
+            case 'archive_by_inactivity': {
+                let archiveInactive = date => {
+                    archiveContacts('leaders_last_sign_in_at')(date);
+                };
+
+                const InactiveModal = $uibModal.open({
+                    component: 'iconModal',
+                    resolve: {
+                        title: () => $filter('t')(`Archive By Inactivity`),
+                        paragraphs: () => [
+                            $filter('t')(
+                                `Are You Sure That You Wish To Archive By Inactivity?`,
+                            ),
+                        ],
+                        icon: () => this.warningIcon,
+                        closeLabel: () => $filter('t')('Yes'),
+                        dismissLabel: () => $filter('t')('No'),
+                    },
+                    windowClass: 'pivot_theme',
+                    backdrop: 'static',
+                    keyboard: false,
+                });
+                // Had to throw in a catch for errors due to an AsyncGenerator.js error I was getting
+                // In the console everytime the modal was dismissed
+                try {
+                    const InactiveResponse = await InactiveModal.result;
+                    archiveInactive(archiveDate);
+                } catch (err) {
+                    return null;
+                }
+
+                break;
+            }
+            case 'archive_by_date': {
+                let archiveBefore = date => {
+                    archiveContacts('persons_inactive_since')(date);
+                };
+                const DateModal = $uibModal.open({
+                    component: 'iconModal',
+                    resolve: {
+                        title: () => $filter('t')(`Archive By Date`),
+                        paragraphs: () => [
+                            $filter('t')(
+                                `Are You Sure That You Wish To Archive By Date?`,
+                            ),
+                        ],
+                        icon: () => this.warningIcon,
+                        closeLabel: () => $filter('t')('Yes'),
+                        dismissLabel: () => $filter('t')('No'),
+                    },
+                    windowClass: 'pivot_theme',
+                    backdrop: 'static',
+                    keyboard: false,
+                });
+                try {
+                    const DateResponse = await DateModal.result;
+                    archiveBefore(archiveDate);
+                } catch (error) {
+                    return null;
+                }
+                break;
+            }
+            default:
+                return null;
+        }
+    };
 
     const archiveContacts = archiveBy => async dateBy => {
         if (!dateBy) return;
-
         const params = {
             type: 'bulk_archive_contacts',
             id: this.orgId,
@@ -72,7 +143,5 @@ function organizationCleanupController(
 
         $scope.$apply();
     };
-
-    this.archiveInactive = archiveContacts('leaders_last_sign_in_at');
-    this.archiveBefore = archiveContacts('persons_inactive_since');
+    this.openModal = confirmModal;
 }
