fe7e18cc1f2a40dc4f2c5db6870d9f60c86b4c49
bwtoptal <bartosz.w@toptal.com>
2019-08-20 19:26:47 +0200

    MHP-2877: Initial TypeScript config

--- src/dashboard/components/Header/index.ts
+++ src/dashboard/components/Header/index.ts
@@ -5,7 +5,7 @@ const Header = styled.h1`
     font-weight: 300;
     font-size: 30px;
     line-height: 38px;
-    color: ${({ theme }) => theme.colors.primary};
+    color: ${(props: any) => props.theme.colors.primary};
     margin-bottom: 0px;
     margin-top: 48px;
     padding-left: 21px;
