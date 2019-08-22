660c91e9015b4661698fea83ebc274eb57ebd53a
bwtoptal <bartosz.w@toptal.com>
2019-08-22 19:54:58 +0200

    MHP-2877: Fixing tests

--- src/dashboard/components/Header/index.ts
+++ src/dashboard/components/Header/index.ts
@@ -1,11 +1,10 @@
 import styled from '@emotion/styled';
-import React from 'react';
 
 const Header = styled.h1`
     font-weight: 300;
     font-size: 30px;
     line-height: 38px;
-    color: ${(props: any) => props.theme.colors.primary};
+    color: ${({ theme }: { theme: any }) => theme.colors.primary};
     margin-bottom: 0px;
     margin-top: 48px;
     padding-left: 21px;
