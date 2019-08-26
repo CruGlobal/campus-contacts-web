68ccb668659d9aaae540a8e33ac8d656e0085ab7
TheNoodleMoose <cjh605@gmail.com>
2019-08-26 10:07:10 -0400

    Added type check to personName filter and deleted assignedPersonName filter

--- app/assets/javascripts/angular/filters/personName.filter.js
+++ app/assets/javascripts/angular/filters/personName.filter.js
@@ -2,14 +2,22 @@
  * The personName filter takes a person and converts it into the person's name.
  */
 angular.module('missionhubApp').filter('personName', function(_) {
-    return function(person) {
+    return function(person, type) {
         if (!person) {
             return null;
         }
-
-        // Ignore missing name parts
-        return [person.first_name, person.last_name]
-            .filter(_.identity)
-            .join(' ');
+        // If there is no type, filter as normal
+        if (!type) {
+            // Ignore missing name parts
+            return [person.first_name, person.last_name]
+                .filter(_.identity)
+                .join(' ');
+        }
+        if (type === 'firstChar') {
+            // Return just the first character of the users last name
+            return [person.first_name, person.last_name.charAt(0)]
+                .filter(_.identity)
+                .join(' ');
+        }
     };
 });
