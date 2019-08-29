4697d22c0195db785dbcd1ce248ba456a5f622a2
TheNoodleMoose <cjh605@gmail.com>
2019-08-29 13:14:00 -0400

    Changed type name and combined conditions into one

--- app/assets/javascripts/angular/filters/personName.filter.js
+++ app/assets/javascripts/angular/filters/personName.filter.js
@@ -6,18 +6,14 @@ angular.module('missionhubApp').filter('personName', function(_) {
         if (!person) {
             return null;
         }
-        // If there is no type, filter as normal
-        if (!type) {
-            // Ignore missing name parts
-            return [person.first_name, person.last_name]
-                .filter(_.identity)
-                .join(' ');
-        }
-        if (type === 'firstChar' && person.last_name) {
-            // Return just the first character of the users last name
-            return [person.first_name, person.last_name.charAt(0)]
-                .filter(_.identity)
-                .join(' ');
-        }
+
+        return [
+            person.first_name,
+            type === 'firstAndLastInitial' && person.last_name
+                ? person.last_name.charAt(0)
+                : person.last_name,
+        ]
+            .filter(_.identity)
+            .join(' ');
     };
 });
