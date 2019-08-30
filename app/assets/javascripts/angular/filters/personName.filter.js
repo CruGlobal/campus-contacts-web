b01b3154135b20e2cea0db5b89702abe3d22974f
TheNoodleMoose <cjh605@gmail.com>
2019-08-30 11:42:14 -0400

    Removed period being always at end of name, even if unassigned

--- app/assets/javascripts/angular/filters/personName.filter.js
+++ app/assets/javascripts/angular/filters/personName.filter.js
@@ -10,7 +10,7 @@ angular.module('missionhubApp').filter('personName', function(_) {
         return [
             person.first_name,
             type === 'firstAndLastInitial' && person.last_name
-                ? person.last_name.charAt(0)
+                ? `${person.last_name.charAt(0)}.`
                 : person.last_name,
         ]
             .filter(_.identity)
