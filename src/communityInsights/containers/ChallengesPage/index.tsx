a92a58ff82c5fe7503d6795373f3cf60ef51d44e
TheNoodleMoose <cjh605@gmail.com>
2019-09-13 11:20:48 -0400

    Finished typing rest of containers

--- src/communityInsights/containers/ChallengesPage/index.tsx
+++ src/communityInsights/containers/ChallengesPage/index.tsx
@@ -8,18 +8,25 @@ import Table from '../../components/Table';
 import ImpactInfo from '../../components/ImpactInfo';
 
 import { GET_IMPACT_CHALLENGES, GET_CHALLENGES } from './queries';
+// GET_IMPACT_CHALLENGES INTERFACES
+import { impactReportStepsCount } from './__generated__/impactReportStepsCount';
+// GET_CHALLENGES INTERFACES
+import {
+    communityChallenges,
+    communityChallenges_community_communityChallenges_nodes as communityChallengesNodes,
+} from './__generated__/communityChallenges';
 
 const ChallengesPage = () => {
     const { t } = useTranslation('insights');
     const { orgId } = useContext(AppContext);
 
-    const mapRows = (data: any) => {
+    const mapRows = (data: communityChallenges) => {
         const percentage = (a: number, b: number) => {
             const value = b === 0 ? 0 : Math.floor((a / b) * 100);
             return `${a} (${value}%)`;
         };
 
-        const duration = (start: Date, end: Date) => {
+        const duration = (start: string, end: string) => {
             const startDate = moment(start).startOf('day');
             const endDate = moment(end).startOf('day');
             const days = Math.floor(
@@ -28,7 +35,7 @@ const ChallengesPage = () => {
             return days === 1 ? '1 Day' : `${days} Days`;
         };
 
-        const range = (start: Date, end: Date) => {
+        const range = (start: string, end: string) => {
             const format = 'MM/DD/YYYY';
             const startDate = moment(start).format(format);
             const endDate = moment(end).format(format);
@@ -43,7 +50,7 @@ const ChallengesPage = () => {
                 completedCount,
                 createdAt,
                 endDate,
-            }: any) => [
+            }: communityChallengesNodes) => [
                 id,
                 title,
                 acceptedCount,
@@ -54,7 +61,7 @@ const ChallengesPage = () => {
         );
     };
 
-    const mapPage = (data: any) => {
+    const mapPage = (data: communityChallenges) => {
         return data.community.communityChallenges.pageInfo;
     };
 
@@ -62,7 +69,7 @@ const ChallengesPage = () => {
         <div>
             <ImpactInfo
                 query={GET_IMPACT_CHALLENGES}
-                text={report =>
+                text={(report: impactReportStepsCount) =>
                     t('challenges.total', {
                         count: report.community.impactReport.stepsCount,
                     })
