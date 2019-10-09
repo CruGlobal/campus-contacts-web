c259697e1318ef5ac7ab2e44961b29b4b538c7ef
TheNoodleMoose <cjh605@gmail.com>
2019-10-09 14:39:56 -0400

    Updated test to use new client_id

--- cypress/integration/tests/missionHub-startup.spec.ts
+++ cypress/integration/tests/missionHub-startup.spec.ts
@@ -29,33 +29,22 @@ describe('Href should link to the key', () => {
 });
 
 Cypress.Commands.add('login', () => {
-    const apiUrl = 'https://api-stage.missionhub.com/apis/v4';
     const theKeyUrl = 'https://thekey.me/cas';
+    const email = 'test@test.com';
+    const password = 'Test1234';
+    const clientID = '5484233800086936290';
 
-    const executionRegex = /<input .*?name="execution".*?value="(.+?)".*?\/>/;
-
-    cy.request(
-        `${theKeyUrl}/login?response_type=token&scope=fullticket&client_id=8138475243408077361&redirect_uri=https%3A%2F%2Fstage.missionhub.com%2Fauth-web`,
-    ).then(({ body }) => {
-        const [_, execution] = executionRegex.exec(body) || [];
-
-        cy.request({
-            method: 'post',
-            url: `${theKeyUrl}/login?response_type=token&scope=fullticket&client_id=8138475243408077361&redirect_uri=https%3A%2F%2Fstage.missionhub.com%2Fauth-web`,
-            form: true,
-            body: {
-                execution,
-                _eventId: 'submit',
-                username: 'test@test.com',
-                password: 'Test1234',
-            },
-        }).then(({ redirects: [redirect] }) => {
-            const accessTokenRegex = /access_token=(.+?)&/;
-            const [_, accessToken] = accessTokenRegex.exec(redirect) || [];
-            cy.visit(
-                `/auth-web#token_type=bearer&access_token=${accessToken}&scope=fullticket&expires_in=3599&thekey_guid=49E1F2F9-55CC-6C10-58FF-B9B46CA79579&thekey_username=test%40test.com`,
-            );
-        });
+    cy.request({
+        method: 'post',
+        url: `${theKeyUrl}/api/oauth/token?grant_type=password&username=${email}&password=${password}&client_id=${clientID}`,
+        form: true,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+    }).then(({ body: { access_token } }) => {
+        cy.visit(
+            `/auth-web#token_type=bearer&access_token=${access_token}&scope=fullticket&expires_in=3599&thekey_guid=49E1F2F9-55CC-6C10-58FF-B9B46CA79579&thekey_username=test%40test.com`,
+        );
     });
 });
 
