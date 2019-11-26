226ea633c23e3739dd67e11902f8783593e5bf95
Shelby Benton <shelby.benton@cru.org>
2019-11-26 08:50:43 -0500

    adding default values to user notification settings

--- app/assets/javascripts/angular/components/user-preferences/user-preferences.component.js
+++ app/assets/javascripts/angular/components/user-preferences/user-preferences.component.js
@@ -10,8 +10,15 @@ class UserPreferences {
         this.$scope = $scope;
         const { user } = this.loggedInPerson.person;
         this.language = user.language;
-        const { person_moved, person_assigned, weekly_digest } =
-            user.notification_settings || {};
+        const {
+            person_moved,
+            person_assigned,
+            weekly_digest,
+        } = user.notification_settings || {
+            person_moved: true,
+            person_assigned: true,
+            weekly_digest: true,
+        };
         this.personMoved = person_moved;
         this.personAssigned = person_assigned;
         this.weeklyDigest = weekly_digest;
